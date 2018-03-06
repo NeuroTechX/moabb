fbf34b41ded0eed38978cd4d22dd0d6875b839f5
Alexandre Barachant <alexandre.barachant@gmail.com>
2018-03-06 15:17:02 -0500

    fix error with example

--- examples/MotorImagery/two_class_motor_imagery.py
+++ examples/MotorImagery/two_class_motor_imagery.py
@@ -1,29 +1,27 @@
-from moabb.contexts.evaluations import * 
+from moabb.contexts.evaluations import WithinSessionEvaluation
 from moabb.contexts.motor_imagery import LeftRightImagery
 from pyriemann.estimation import Covariances
 from pyriemann.spatialfilters import CSP
-from pyriemann.classification import TSclassifier, MDM
+from pyriemann.classification import TSclassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.svm import SVC
 
 from collections import OrderedDict
 from moabb import utils
-from moabb.viz import meta_analysis as ma
 from moabb.viz import analyze
 
-datasets = utils.dataset_search('imagery',events=['right_hand','left_hand'],
-                                exact_events=True, min_subjects=2, multi_session=False)
+datasets = utils.dataset_search('imagery', events=['right_hand', 'left_hand'],
+                                has_all_events=True, min_subjects=2,
+                                multi_session=False)
 
 pipelines = OrderedDict()
 pipelines['TS'] = make_pipeline(Covariances('oas'), TSclassifier())
 pipelines['CSP+LDA'] = make_pipeline(Covariances('oas'), CSP(8), LDA())
-pipelines['CSP+SVM'] = make_pipeline(Covariances('oas'), CSP(8), SVC())  # 
+pipelines['CSP+SVM'] = make_pipeline(Covariances('oas'), CSP(8), SVC())  #
 
 context = LeftRightImagery(pipelines, WithinSessionEvaluation(), datasets)
 
 results = context.process()
 
-results = context.process()
 analyze(results, '')
-
