975e87b0419597d81b62fe5c3cf667f296007ddf
Alexandre Barachant <alexandre.barachant@gmail.com>
2018-03-07 18:13:34 -0500

    update test

--- moabb/tests/contexts.py
+++ moabb/tests/contexts.py
@@ -7,7 +7,6 @@ import unittest
 from pyriemann.spatialfilters import CSP
 from pyriemann.estimation import Covariances
 from sklearn.pipeline import make_pipeline
-import os
 
 from collections import OrderedDict
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
@@ -15,8 +14,6 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 pipelines = OrderedDict()
 pipelines['C'] = make_pipeline(Covariances('oas'), CSP(8), LDA())
 dataset = BNCI2014001()
-dataset.selected_events = {k: dataset.event_id[k]
-                           for k in ['left_hand', 'right_hand']}
 
 
 class Test_CrossSess(unittest.TestCase):
@@ -26,12 +23,10 @@ class Test_CrossSess(unittest.TestCase):
     run it. Putting this on the future docket...
 
     '''
-    def tearDown(self):
-        if os.path.isfile('results.hd5'):
-            os.remove('results.hd5')
 
     def return_eval(self):
-        return ev.CrossSessionEvaluation(paradigm=LeftRightImagery())
+        return ev.CrossSessionEvaluation(paradigm=LeftRightImagery(),
+                                         datasets=[dataset])
 
     def test_eval_results(self):
         e = self.return_eval()
@@ -41,12 +36,14 @@ class Test_CrossSess(unittest.TestCase):
 
 class Test_CrossSubj(Test_CrossSess):
     def return_eval(self):
-        return ev.CrossSubjectEvaluation(paradigm=LeftRightImagery())
+        return ev.CrossSubjectEvaluation(paradigm=LeftRightImagery(),
+                                         datasets=[dataset])
 
 
 class Test_WithinSess(Test_CrossSess):
     def return_eval(self):
-        return ev.WithinSessionEvaluation(paradigm=LeftRightImagery())
+        return ev.WithinSessionEvaluation(paradigm=LeftRightImagery(),
+                                          datasets=[dataset])
 
 
 if __name__ == "__main__":
