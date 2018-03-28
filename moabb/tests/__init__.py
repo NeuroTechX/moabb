37e639c62572c240516922d5e3a99aba9df60a26
Alexandre Barachant <alexandre.barachant@gmail.com>
2018-03-28 00:38:54 -0400

    enable tests again

--- moabb/tests/__init__.py
+++ moabb/tests/__init__.py
@@ -1,9 +1,8 @@
-
+from moabb.tests.analysis import *
+# from moabb.tests.datasets import * # skip this for travis
+from moabb.tests.contexts import *
+# from moabb.tests.util_tests import *
 import unittest
 
 if __name__ == "__main__":
-    from moabb.tests.analysis import *
-    # from moabb.tests.datasets import * # skip this for travis
-    from moabb.tests.contexts import *
-    # from moabb.tests.util_tests import *
     unittest.main()
