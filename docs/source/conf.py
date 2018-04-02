24087db85cd52a034069b66b4901a19a347957da
Alexandre Barachant <alexandre.barachant@gmail.com>
2018-04-01 20:41:53 -0400

    add moabb path for doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,8 @@ import sphinx_bootstrap_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-
+sys.path.insert(0, os.path.abspath('../../'))
+import moabb
 # -- Project information -----------------------------------------------------
 
 project = 'moabb'
