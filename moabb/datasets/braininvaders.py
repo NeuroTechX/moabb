c225482527073dd6130f81ead352669f00abb7fd
Pedro Coelho <pedro-luiz.coelho-rodrigues@gipsa-lab.fr>
2018-11-23 11:55:50 +0100

    new documentation

--- moabb/datasets/braininvaders.py
+++ moabb/datasets/braininvaders.py
@@ -9,49 +9,96 @@ import yaml
 BI2013a_URL = 'https://zenodo.org/record/1494240/files/'
 
 
-class BrainInvaders2013a(BaseDataset):
-    '''P300 dataset from a Brain Invaders experiment in 2013.
+class bi2013a(BaseDataset):
+    '''P300 dataset bi2013a from a "Brain Invaders" experiment (2013)
+    carried-out at University of Grenoble Alpes.
 
     Dataset following the setup from [1]_.
 
     **Dataset Description**
 
-    This dataset concerns an experiment carried out at GIPSA-lab in Grenoble
-    using the Brain Invaders P300-based Brain-Computer Interface [1].
-    The recordings were done on 24 subjects in total. Subjects 1 to 7
-    participated in 8 sessions and the others only did one session.
+    This dataset concerns an experiment carried out at GIPSA-lab
+    (University of Grenoble Alpes, CNRS, Grenoble-INP) in 2013.
+    Principal Investigators: Erwan Vaineau, Dr. Alexandre Barachant
+    Scientific Supervisor :  Dr. Marco Congedo
+    Technical Supervisor : Anton Andreev
 
-    Each session consisted in a NonAdaptive and an Adaptive experimental
-    condition, which had a Training (calibration) phase and an Online phase.
-    For both conditions, the data from the Training phase was used for
-    classifying the trials on the Online phase, which served as a feedback for
-    the user. However, in the Adaptive experimental condition, the new trials
-    available during the Online phase were also used for the classification.
+    The experiment uses the Brain Invaders P300-based Brain-Computer Interface
+    [7], which uses the Open-ViBE platform for on-line EEG data acquisition and
+    processing [1, 9]. For classification purposes the Brain Invaders implements
+    on-line Riemannian MDM classifiers [2, 3, 4, 6]. This experiment features
+    both a training-test (classical) mode of operation and a calibration-less
+    mode of operation [4, 5, 6].
 
-    Because of the experimental setup of the Brain Invaders paradigm, there
-    is 1 Target trial to 5 NonTarget trials (i.e., the classes are unbalanced).
-    This means that for quantifying the performance of a classification method
-    using this dataset one should prefer the AUC score instead of accuracy.
+    The recordings concerned 24 subjects in total. Subjects 1 to 7 participated
+    to eight sessions, run in different days, subject 8 to 24 participated to
+    one session. Each session consisted in two runs, one in a Non-Adaptive
+    (classical) and one in an Adaptive (calibration-less) mode of operation.
+    The order of the runs was randomized for each session. In both runs there
+    was a Training (calibration) phase and an Online phase, always passed in
+    this order. In the non-Adaptive run the data from the Training phase was
+    used for classifying the trials on the Online phase using the training-test
+    version of the MDM algorithm [3, 4]. In the Adaptive run, the data from the
+    training phase was not used at all, instead the classifier was initialized
+    with generic class geometric means and continuously adapted to the incoming
+    data using the Riemannian method explained in [4]. Subjects were completely
+    blind to the mode of operation and the two runs appeared to them identical.
 
-    Data were acquired with a sampling frequency of 512 Hz and using
-    16 electrodes at positions FP1, FP2, F5, AFz, F6, T7, Cz, T8, P7, P3, Pz,
-    P4, P8, O1, Oz, O2 according to the 10/20 international system, using as
-    reference the left ear-lobe.
+    In the Brain Invaders P300 paradigm, a repetition is composed of 12 flashes,
+    of which 2 include the Target symbol (Target flashes) and 10 do not
+    (non-Target flash). Please see [7] for a description of the paradigm. For
+    this experiment, in the Training phases the number of flashes is fixed
+    (80 Target flashes and 400 non-Target flashes). In the Online phases the
+    number of Target and non-Target still are in a ratio 1/5, however their
+    number is variable because the Brain Invaders works with a fixed number of
+    game levels, however the number of repetitions needed to destroy the target
+    (hence to proceed to the next level) depends on the user’s performance
+    [4, 5]. In any case, since the classes are unbalanced, an appropriate score
+    must be used for quantifying the performance of classification methods
+    (e.g., balanced accuracy, AUC methods, etc).
 
-    The reader may check Ref.[2] for an example of study using this dataset.
+    Data were acquired with a Nexus (TMSi, The Netherlands) EEG amplifier:
+    - Sampling Frequency: 512 samples per second
+    - Digital Filter: no
+    - Electrodes:  16 wet Silver/Silver Chloride electrodes positioned at
+      FP1, FP2, F5, AFz, F6, T7, Cz, T8, P7, P3, Pz, P4, P8, O1, Oz, O2
+      according to the 10/20 international system.
+    - Reference: left ear-lobe.
+    - Ground: N/A.
 
     References
     ----------
 
-    .. [1] Congedo, M., Goyat, M., Tarrin, N., Ionescu, G., Rivet, B.,
-           Varnet, L., Rivet, B., Phlypo, R., Jrad, N., Acquadro, M.,
-           Jutten, C., Brain Invaders : a prototype of an open-source
-           P300-based video game working with the OpenViBE.
-           Proc. IBCI Conf., Graz, Austria, 280-283.
-           https://doi.org/10.1016/j.jneumeth.2007.03.005
-
-    .. [2] Barachant, A., Congedo, M., A Plug & Play P300 BCI using Information
-           Geometry, 2014, Available at arXiv (ref. arXiv:1409.0107)
+    .. [1] Arrouët C, Congedo M, Marvie J-E, Lamarche F, Lècuyer A, Arnaldi B
+           (2005) Open-ViBE: a 3D Platform for Real-Time Neuroscience.
+           Journal of Neurotherapy, 9(1), 3-25.
+    .. [2] Barachant A, Bonnet S, Congedo M, Jutten C (2013) Classification of
+           covariance matrices using a Riemannian-based kernel for BCI
+           applications. Neurocomputing 112, 172-178.
+    .. [3] Barachant A, Bonnet S, Congedo M, Jutten C (2012) Multi-Class Brain
+           Computer Interface, Classification by Riemannian Geometry.
+           IEEE Transactions on Biomedical Engineering 59(4), 920-928
+    .. [4] Barachant A, Congedo M (2014) A Plug & Play P300 BCI using
+           Information Geometry.
+           arXiv:1409.0107.
+    .. [5] Congedo M, Barachant A, Andreev A (2013) A New Generation of
+           Brain-Computer Interface Based on Riemannian Geometry.
+           arXiv:1310.8115.
+    .. [6] Congedo M, Barachant A, Bhatia R (2017) Riemannian Geometry for
+           EEG-based Brain-Computer Interfaces; a Primer and a Review.
+           Brain-Computer Interfaces, 4(3), 155-174.
+    .. [7] Congedo M, Goyat M, Tarrin N, Ionescu G, Rivet B,Varnet L, Rivet B,
+           Phlypo R, Jrad N, Acquadro M, Jutten C (2011) “Brain Invaders”: a
+           prototype of an open-source P300-based video game working with the
+           OpenViBE platform. Proc. IBCI Conf., Graz, Austria, 280-283.
+    .. [8] Congedo M, Korczowski L, Delorme A, Lopes da Silva F. (2016)
+           Spatio-temporal common pattern: A companion method for ERP analysis
+           in the time domain. Journal of Neuroscience Methods, 267, 74-88.
+    .. [9] Renard Y, Lotte F, Gibert G, Congedo M, Maby E, Delannoy V, Bertrand
+           O, Lécuyer A (2010) OpenViBE: An Open-Source Software Platform to
+           Design, Test and Use Brain-Computer Interfaces in Real and Virtual
+           Environments. PRESENCE : Teleoperators and Virtual Environments
+           19(1), 35-53.
     '''
 
     def __init__(
