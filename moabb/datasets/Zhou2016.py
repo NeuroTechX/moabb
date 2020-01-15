fc2d4135f61b23d2cfbaaf22a4c6b101c4b0d369
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2020-01-15 16:58:15 +0100

    correcting montage

--- moabb/datasets/Zhou2016.py
+++ moabb/datasets/Zhou2016.py
@@ -6,6 +6,7 @@ https://doi.org/10.1371/journal.pone.0114853
 from .base import BaseDataset
 import zipfile as z
 from mne.io import read_raw_cnt
+from mne.channels import make_standard_montage
 from mne.datasets.utils import _get_path, _do_path_update
 from mne.utils import _fetch_file
 import os
@@ -92,8 +93,9 @@ class Zhou2016(BaseDataset):
             out[sess_key] = {}
             for run_ind, fname in enumerate(runlist):
                 run_key = 'run_{}'.format(run_ind)
-                out[sess_key][run_key] = read_raw_cnt(fname, preload=True,
-                                                      montage='standard_1005')
+                out[sess_key][run_key] = read_raw_cnt(fname, preload=True)
+                out[sess_key][run_key].set_montage(
+                    make_standard_montage('standard_1005'))
         return out
 
     def data_path(self, subject, path=None, force_update=False,
