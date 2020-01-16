baef901a4318c862dfea08cf11cc6bf4f90baafa
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2020-01-16 11:36:23 +0100

    adding missing ERP datasets

--- moabb/tests/download.py
+++ moabb/tests/download.py
@@ -5,7 +5,8 @@ from moabb.datasets.gigadb import Cho2017
 from moabb.datasets.alex_mi import AlexMI
 from moabb.datasets.physionet_mi import PhysionetMI
 from moabb.datasets.bnci import (BNCI2014001, BNCI2014002, BNCI2014004,
-                                 BNCI2015001, BNCI2015004)
+                                 BNCI2014008, BNCI2014009, BNCI2015001,
+                                 BNCI2015003, BNCI2015004)
 from moabb.datasets.bbci_eeg_fnirs import Shin2017A, Shin2017B
 from moabb.datasets.upper_limb import Ofner2017
 from moabb.datasets.mpi_mi import MunichMI
@@ -13,6 +14,7 @@ from moabb.datasets.schirrmeister2017 import Schirrmeister2017
 from moabb.datasets.Weibo2014 import Weibo2014
 from moabb.datasets.Zhou2016 import Zhou2016
 from moabb.datasets.ssvep_exo import SSVEPExo
+from moabb.datasets.braininvaders import bi2013a
 import unittest
 import mne
 
@@ -42,7 +44,8 @@ class Test_Downloads(unittest.TestCase):
         # session must be a dict, and the length must match
         for _, sessions in data.items():
             self.assertTrue(isinstance(sessions, dict))
-            self.assertTrue(len(sessions) == obj.n_sessions)
+            self.assertEqual(len(sessions), obj.n_sessions)
+            self.assertTrue(len(sessions) >= obj.n_sessions)
 
             # each session is a dict, with multiple runs
             for _, runs in sessions.items():
