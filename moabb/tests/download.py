ee5909ed5bb849fd2e389168cf1b4ba16963095b
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2020-06-24 05:28:29 +0200

    removing blocking download test from TravisCI

--- moabb/tests/download.py
+++ moabb/tests/download.py
@@ -60,60 +60,60 @@ class Test_Downloads(unittest.TestCase):
     # def test_cho2017(self):
     #     self.run_dataset(Cho2017)
 
-    def test_bnci_1401(self):
-        self.run_dataset(BNCI2014001)
+    # def test_bnci_1401(self):
+    #     self.run_dataset(BNCI2014001)
 
-    def test_bnci_1402(self):
-        self.run_dataset(BNCI2014002)
+    # def test_bnci_1402(self):
+    #     self.run_dataset(BNCI2014002)
 
-    def test_bnci_1404(self):
-        self.run_dataset(BNCI2014004)
+    # def test_bnci_1404(self):
+    #     self.run_dataset(BNCI2014004)
 
-    def test_bnci_1408(self):
-        self.run_dataset(BNCI2014008)
+    # def test_bnci_1408(self):
+    #     self.run_dataset(BNCI2014008)
 
-    def test_bnci_1409(self):
-        self.run_dataset(BNCI2014009)
+    # def test_bnci_1409(self):
+    #     self.run_dataset(BNCI2014009)
 
-    def test_bnci_1501(self):
-        self.run_dataset(BNCI2015001)
+    # def test_bnci_1501(self):
+    #     self.run_dataset(BNCI2015001)
 
-    def test_bnci_1503(self):
-        self.run_dataset(BNCI2015003)
+    # def test_bnci_1503(self):
+    #     self.run_dataset(BNCI2015003)
 
-    def test_bnci_1504(self):
-        self.run_dataset(BNCI2015004)
+    # def test_bnci_1504(self):
+    #     self.run_dataset(BNCI2015004)
 
-    def test_alexmi(self):
-        self.run_dataset(AlexMI)
+    # def test_alexmi(self):
+    #     self.run_dataset(AlexMI)
 
-    def test_physionet(self):
-        self.run_dataset(PhysionetMI)
+    # def test_physionet(self):
+    #     self.run_dataset(PhysionetMI)
 
-    def test_eegfnirs(self):
-        self.run_dataset(Shin2017A)
-        self.run_dataset(Shin2017B)
+    # def test_eegfnirs(self):
+    #     self.run_dataset(Shin2017A)
+    #     self.run_dataset(Shin2017B)
 
-    def test_upper_limb(self):
-        self.run_dataset(Ofner2017)
+    # def test_upper_limb(self):
+    #     self.run_dataset(Ofner2017)
 
-    def test_mpi_mi(self):
-        self.run_dataset(MunichMI)
+    # def test_mpi_mi(self):
+    #     self.run_dataset(MunichMI)
 
-    def test_schirrmeister2017(self):
-        self.run_dataset(Schirrmeister2017, subj=(0, 1))
+    # def test_schirrmeister2017(self):
+    #     self.run_dataset(Schirrmeister2017, subj=(0, 1))
 
-    def test_Weibo2014(self):
-        self.run_dataset(Weibo2014)
+    # def test_Weibo2014(self):
+    #     self.run_dataset(Weibo2014)
 
-    def test_Zhou2016(self):
-        self.run_dataset(Zhou2016)
+    # def test_Zhou2016(self):
+    #     self.run_dataset(Zhou2016)
 
-    def test_ssvep_exo(self):
-        self.run_dataset(SSVEPExo)
+    # def test_ssvep_exo(self):
+    #     self.run_dataset(SSVEPExo)
 
-    def test_bi2013a(self):
-        self.run_dataset(bi2013a)
+    # def test_bi2013a(self):
+    #     self.run_dataset(bi2013a)
 
 
 if __name__ == '__main__':
