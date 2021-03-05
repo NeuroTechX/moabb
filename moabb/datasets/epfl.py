9710531479bb7b0e2efbd89705916725034d3b74
Vladislav Goncharenko <vladislav.goncharenko@phystech.edu>
2021-03-06 02:04:33 +0300

    refomatted some p300 datasets

--- moabb/datasets/epfl.py
+++ moabb/datasets/epfl.py
@@ -12,7 +12,7 @@ from moabb.datasets import download as dl
 from moabb.datasets.base import BaseDataset
 
 
-EPFLP300_URL = 'http://documents.epfl.ch/groups/m/mm/mmspg/www/BCI/p300/'
+EPFLP300_URL = "http://documents.epfl.ch/groups/m/mm/mmspg/www/BCI/p300/"
 
 
 class EPFLP300(BaseDataset):
@@ -66,64 +66,36 @@ class EPFLP300(BaseDataset):
             subjects=[1, 2, 3, 4, 6, 7, 8, 9],
             sessions_per_subject=4,
             events=dict(Target=2, NonTarget=1),
-            code='EPFL P300 dataset',
+            code="EPFL P300 dataset",
             interval=[0, 1],
-            paradigm='p300',
-            doi='10.1016/j.jneumeth.2007.03.005')
+            paradigm="p300",
+            doi="10.1016/j.jneumeth.2007.03.005",
+        )
 
     def _get_single_run_data(self, file_path):
 
         # data from the .mat
         data = loadmat(file_path)
-        signals = data['data']
-        stimuli = data['stimuli'].squeeze()
-        events = data['events']
-        target = data['target'][0][0]
+        signals = data["data"]
+        stimuli = data["stimuli"].squeeze()
+        events = data["events"]
+        target = data["target"][0][0]
 
         # meta-info from the readme.pdf
         sfreq = 2048
+        # fmt: off
         ch_names = [
-            'Fp1',
-            'AF3',
-            'F7',
-            'F3',
-            'FC1',
-            'FC5',
-            'T7',
-            'C3',
-            'CP1',
-            'CP5',
-            'P7',
-            'P3',
-            'Pz',
-            'PO3',
-            'O1',
-            'Oz',
-            'O2',
-            'PO4',
-            'P4',
-            'P8',
-            'CP6',
-            'CP2',
-            'C4',
-            'T8',
-            'FC6',
-            'FC2',
-            'F4',
-            'F8',
-            'AF4',
-            'Fp2',
-            'Fz',
-            'Cz',
-            'MA1',
-            'MA2']
-        ch_types = ['eeg'] * 32 + ['misc'] * 2
+            "Fp1", "AF3", "F7", "F3", "FC1", "FC5", "T7", "C3", "CP1", "CP5", "P7", "P3",
+            "Pz", "PO3", "O1", "Oz", "O2", "PO4", "P4", "P8", "CP6", "CP2", "C4", "T8",
+            "FC6", "FC2", "F4", "F8", "AF4", "Fp2", "Fz", "Cz", "MA1", "MA2",
+        ]
+        # fmt: on
+        ch_types = ["eeg"] * 32 + ["misc"] * 2
 
         # The last X entries are 0 for all signals. This leads to
         # artifacts when epoching and band-pass filtering the data.
         # Correct the signals for this.
-        sig_i = np.where(
-            np.diff(np.all(signals == 0, axis=0).astype(int)) != 0)[0][0]
+        sig_i = np.where(np.diff(np.all(signals == 0, axis=0).astype(int)) != 0)[0][0]
         signals = signals[:, :sig_i]
         signals *= 1e-6  # data is stored as uV, but MNE expects V
         # we have to re-reference the signals
@@ -135,15 +107,15 @@ class EPFLP300(BaseDataset):
         # getting the event time in a Python standardized way
         events_datetime = []
         for eventi in events:
-            events_datetime.append(dt.datetime(
-                *eventi.astype(int), int(eventi[-1] * 1e3) % 1000 * 1000))
+            events_datetime.append(
+                dt.datetime(*eventi.astype(int), int(eventi[-1] * 1e3) % 1000 * 1000)
+            )
 
         # get the indices of the stimuli
         pos = []
         n_trials = len(stimuli)
         for j in range(n_trials):
-            delta_seconds = (
-                events_datetime[j] - events_datetime[0]).total_seconds()
+            delta_seconds = (events_datetime[j] - events_datetime[0]).total_seconds()
             delta_indices = int(delta_seconds * sfreq)
             # has to add an offset
             pos.append(delta_indices + int(0.4 * sfreq))
@@ -154,17 +126,17 @@ class EPFLP300(BaseDataset):
         stim_aux[stimuli != target] = 1
         stim_channel = np.zeros(signals.shape[1])
         stim_channel[pos] = stim_aux
-        ch_names = ch_names + ['STI']
-        ch_types = ch_types + ['stim']
+        ch_names = ch_names + ["STI"]
+        ch_types = ch_types + ["stim"]
         signals = np.concatenate([signals, stim_channel[None, :]])
 
         # create info dictionary
         info = mne.create_info(ch_names, sfreq, ch_types)
-        info['description'] = 'EPFL P300 dataset'
+        info["description"] = "EPFL P300 dataset"
 
         # create the Raw structure
         raw = mne.io.RawArray(signals, info, verbose=False)
-        montage = make_standard_montage('biosemi32')
+        montage = make_standard_montage("biosemi32")
         raw.set_montage(montage)
 
         return raw
@@ -177,43 +149,36 @@ class EPFLP300(BaseDataset):
 
         for file_path in sorted(file_path_list):
 
-            session_name = 'session_' + \
-                file_path.split(os.sep)[-2].replace('session', '')
+            session_name = "session_" + file_path.split(os.sep)[-2].replace("session", "")
 
             if session_name not in sessions.keys():
                 sessions[session_name] = {}
 
-            run_name = 'run_' + str(len(sessions[session_name]) + 1)
-            sessions[session_name][run_name] = self._get_single_run_data(
-                file_path)
+            run_name = "run_" + str(len(sessions[session_name]) + 1)
+            sessions[session_name][run_name] = self._get_single_run_data(file_path)
 
         return sessions
 
     def data_path(
-            self,
-            subject,
-            path=None,
-            force_update=False,
-            update_path=None,
-            verbose=None):
+        self, subject, path=None, force_update=False, update_path=None, verbose=None
+    ):
 
         if subject not in self.subject_list:
-            raise(ValueError("Invalid subject number"))
+            raise (ValueError("Invalid subject number"))
 
         # check if has the .zip
-        url = '{:s}subject{:d}.zip'.format(EPFLP300_URL, subject)
-        path_zip = dl.data_path(url, 'EPFLP300')
-        path_folder = path_zip.strip('subject{:d}.zip'.format(subject))
+        url = "{:s}subject{:d}.zip".format(EPFLP300_URL, subject)
+        path_zip = dl.data_path(url, "EPFLP300")
+        path_folder = path_zip.strip("subject{:d}.zip".format(subject))
 
         # check if has to unzip
-        if not(os.path.isdir(path_folder + 'subject{:d}'.format(subject))):
-            print('unzip', path_zip)
+        if not (os.path.isdir(path_folder + "subject{:d}".format(subject))):
+            print("unzip", path_zip)
             zip_ref = zipfile.ZipFile(path_zip, "r")
             zip_ref.extractall(path_folder)
 
         # get the path to all files
-        pattern = os.path.join('subject{:d}'.format(subject), '*', '*')
-        subject_paths = glob.glob(
-            path_folder + pattern)
+        pattern = os.path.join("subject{:d}".format(subject), "*", "*")
+        subject_paths = glob.glob(path_folder + pattern)
 
         return subject_paths
