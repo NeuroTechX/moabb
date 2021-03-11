9bdfa5a46bd563de8075bcb2429602169f6358c2
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-03-11 17:19:20 +0100

    code linting

--- moabb/datasets/gigadb.py
+++ moabb/datasets/gigadb.py
@@ -91,7 +91,7 @@ class Cho2017(BaseDataset):
             "P9", "PO7", "PO3", "O1", "Iz", "Oz", "POz", "Pz", "CPz", "Fpz", "Fp2",
             "AF8", "AF4", "AFz", "Fz", "F2", "F4", "F6", "F8", "FT8", "FC6", "FC4",
             "FC2", "FCz", "Cz", "C2", "C4", "C6", "T8", "TP8", "CP6", "CP4", "CP2",
-            "P2", "P4", "P6", "P8", "P10", "PO8", "PO4", "O2", 
+            "P2", "P4", "P6", "P8", "P10", "PO8", "PO4", "O2",
         ]
         # fmt: on
         emg_ch_names = ['EMG1', 'EMG2', 'EMG3', 'EMG4']
