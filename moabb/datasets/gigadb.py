711501817be2e0fd4d5f0ba25921f5679fbafe59
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-03-11 17:13:39 +0100

    correct formatting for gigadb

--- moabb/datasets/gigadb.py
+++ moabb/datasets/gigadb.py
@@ -85,14 +85,14 @@ class Cho2017(BaseDataset):
         )['eeg']
 
         # fmt: off
-        eeg_ch_names = ['Fp1', 'AF7', 'AF3', 'F1', 'F3', 'F5', 'F7', 'FT7',
-                        'FC5', 'FC3', 'FC1', 'C1', 'C3', 'C5', 'T7', 'TP7',
-                        'CP5', 'CP3', 'CP1', 'P1', 'P3', 'P5', 'P7', 'P9',
-                        'PO7', 'PO3', 'O1', 'Iz', 'Oz', 'POz', 'Pz', 'CPz',
-                        'Fpz', 'Fp2', 'AF8', 'AF4', 'AFz', 'Fz', 'F2', 'F4',
-                        'F6', 'F8', 'FT8', 'FC6', 'FC4', 'FC2', 'FCz', 'Cz',
-                        'C2', 'C4', 'C6', 'T8', 'TP8', 'CP6', 'CP4', 'CP2',
-                        'P2', 'P4', 'P6', 'P8', 'P10', 'PO8', 'PO4', 'O2']
+        eeg_ch_names = [
+            "Fp1", "AF7", "AF3", "F1", "F3", "F5", "F7", "FT7", "FC5", "FC3", "FC1",
+            "C1", "C3", "C5", "T7", "TP7", "CP5", "CP3", "CP1", "P1", "P3", "P5", "P7",
+            "P9", "PO7", "PO3", "O1", "Iz", "Oz", "POz", "Pz", "CPz", "Fpz", "Fp2",
+            "AF8", "AF4", "AFz", "Fz", "F2", "F4", "F6", "F8", "FT8", "FC6", "FC4",
+            "FC2", "FCz", "Cz", "C2", "C4", "C6", "T8", "TP8", "CP6", "CP4", "CP2",
+            "P2", "P4", "P6", "P8", "P10", "PO8", "PO4", "O2", 
+        ]
         # fmt: on
         emg_ch_names = ['EMG1', 'EMG2', 'EMG3', 'EMG4']
         ch_names = eeg_ch_names + emg_ch_names + ['Stim']
