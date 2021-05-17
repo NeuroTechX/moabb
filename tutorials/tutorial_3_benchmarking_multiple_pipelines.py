44f12cbfe2dcc7e87f687996fc8866d8b3443adf
Sylvain Chevallier <sylvain.chevallier@uvsq.fr>
2021-05-17 22:24:08 +0200

    correct encoding, limit subject, update text
    Signed-off-by: Sylvain Chevallier <sylvain.chevallier@uvsq.fr>

--- tutorials/tutorial_3_benchmarking_multiple_pipelines.py
+++ tutorials/tutorial_3_benchmarking_multiple_pipelines.py
@@ -9,12 +9,10 @@ classification score of not one but three classification pipelines.
 #
 # https://github.com/plcrodrigues/Workshop-MOABB-BCI-Graz-2019
 
-import os
 import warnings
 
 import matplotlib.pyplot as plt
 import mne
-import pandas as pd
 import seaborn as sns
 from mne.decoding import CSP
 from pyriemann.classification import MDM
@@ -25,7 +23,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
 
 import moabb
-from moabb.datasets import BNCI2014001, Weibo2014, Zhou2016
+from moabb.datasets import BNCI2014001, Zhou2016
 from moabb.evaluations import WithinSessionEvaluation
 from moabb.paradigms import LeftRightImagery
 
@@ -52,16 +50,21 @@ pipelines["tgsp+svm"] = make_pipeline(
 )
 pipelines["MDM"] = make_pipeline(Covariances("oas"), MDM(metric="riemann"))
 
-# The following lines go exactly as in the previous example, where we end up
+# The following lines go exactly as in the previous tutorial, where we end up
 # obtaining a pandas dataframe containing the results of the evaluation.
-datasets = [BNCI2014001(), Weibo2014(), Zhou2016()]
+datasets = [BNCI2014001(), Zhou2016()]
+subj = [1, 2, 3]
+for d in datasets:
+    d.subject_list = subj
 paradigm = LeftRightImagery()
-evaluation = WithinSessionEvaluation(paradigm=paradigm, datasets=datasets, overwrite=True)
+evaluation = WithinSessionEvaluation(
+    paradigm=paradigm, datasets=datasets, overwrite=False
+)
 results = evaluation.process(pipelines)
-if not os.path.exists("./results"):
-    os.mkdir("./results")
-results.to_csv("./results/results_part2-3.csv")
-results = pd.read_csv("./results/results_part2-3.csv")
+
+# As `overwrite` is set to False, the results from the previous tutorial are reused and
+# only the new pipelines are evaluated. The results from "csp+lda" are not recomputed.
+# The results are saved in ~/mne_data/results if the parameter `hdf5_path` is not set.
 
 ##############################################################################
 # Plotting Results
