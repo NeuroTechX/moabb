07639098ee360fd657cff5469b7c9f77bf02d692
GUETSCHEL Pierre <pierre.guetschel@inria.fr>
2021-07-06 12:03:52 +0200

    Seperate train and test runs

--- moabb/datasets/Lee2019.py
+++ moabb/datasets/Lee2019.py
@@ -47,6 +47,15 @@ class Lee2019_MI(BaseDataset):
     center of the monitor and moved right or left, according to the
     real-time classifier output of the EEG signal.
 
+    Parameters
+    ----------
+
+    train_run: bool (default True)
+        if True, return runs corresponding to the training/offline phase (see paper).
+
+    test_run: bool (default False)
+        if True, return runs corresponding to the test/online phase (see paper).
+
     References
     ----------
     .. [1] Lee, M. H., Kwon, O. Y., Kim, Y. J., Kim, H. K., Lee, Y. E.,
@@ -56,7 +65,7 @@ class Lee2019_MI(BaseDataset):
            https://doi.org/10.1093/gigascience/giz002
     """
 
-    def __init__(self):
+    def __init__(self, train_run=True, test_run=False):
         super().__init__(
             subjects=list(range(1, 55)),
             sessions_per_subject=2,
@@ -66,6 +75,39 @@ class Lee2019_MI(BaseDataset):
             paradigm="imagery",
             doi="10.5524/100542",
         )
+        self.train_run = train_run
+        self.test_run  =  test_run
+
+    def _get_single_run(self, data):
+        # Create channel info and montage
+        eeg_ch_names = data['chan'][0]
+        ch_names = [elem[0] for elem in eeg_ch_names] + ["stim"]
+        ch_types = ["eeg"] * 62 + ["stim"]
+        sfreq = data['fs'][0, 0]
+        info = create_info(ch_names=ch_names, ch_types=ch_types, sfreq=sfreq)
+        montage = make_standard_montage("standard_1005")
+
+        # Create raw_data
+        raw_data = np.transpose(data['smt'], (1, 2, 0))
+
+        # Create raw_event
+        event_id = data['y_dec'].ravel()
+        raw_events = np.zeros((raw_data.shape[0], 1, raw_data.shape[2]))
+        raw_events[:, 0, 0] = event_id
+
+        # Zero pad the data
+        data = np.concatenate([raw_data, raw_events], axis=1)
+        zeroshape = (data.shape[0], data.shape[1], 50)
+        data = np.concatenate(
+            [np.zeros(zeroshape), data, np.zeros(zeroshape)], axis=2
+        )
+
+        # Create RawArray
+        raw = RawArray(
+            data=np.concatenate(list(data), axis=1), info=info, verbose=False
+        )
+        raw.set_montage(montage)
+        return raw
 
     def _get_single_subject_data(self, subject):
         """return data for a single subejct"""
@@ -74,44 +116,15 @@ class Lee2019_MI(BaseDataset):
         file_path_list = self.data_path(subject)
 
         for session in range(1, 3):
-            data = loadmat(file_path_list[session - 1])
+            if self.train_run or self.test_run:
+                mat = loadmat(file_path_list[session - 1])
 
-            # Create channel info and montage
-            eeg_ch_names = data["EEG_MI_train"][0, 0]['chan'][0]
-            ch_names = [elem[0] for elem in eeg_ch_names] + ["stim"]
-            ch_types = ["eeg"] * 62 + ["stim"]
-            sfreq = data["EEG_MI_train"][0, 0]['fs'][0, 0]
-            info = create_info(ch_names=ch_names, ch_types=ch_types, sfreq=sfreq)
-            montage = make_standard_montage("standard_1005")
-
-            # Create raw_data
-            raw_train_data = np.transpose(data["EEG_MI_train"][0, 0]['smt'], (1, 2, 0))
-            raw_test_data = np.transpose(data["EEG_MI_test"][0, 0]['smt'], (1, 2, 0))
-            raw_data = np.concatenate([raw_train_data, raw_test_data], axis=0)
-
-            # Create raw_event
-            train_event_id = data["EEG_MI_train"][0, 0]['y_dec'].ravel()
-            test_event_id = data["EEG_MI_test"][0, 0]['y_dec'].ravel()
-            event_id = np.concatenate([train_event_id, test_event_id], axis=0)
-            raw_events = np.zeros((raw_data.shape[0], 1, raw_data.shape[2]))
-            raw_events[:, 0, 0] = event_id
-
-            # Zero pad the data
-            data = np.concatenate([raw_data, raw_events], axis=1)
-            zeroshape = (data.shape[0], data.shape[1], 50)
-            data = np.concatenate(
-                [np.zeros(zeroshape), data, np.zeros(zeroshape)], axis=2
-            )
-
-            # Create RawArray
-            raw = RawArray(
-                data=np.concatenate(list(data), axis=1), info=info, verbose=False
-            )
-            raw.set_montage(montage)
-
-            # add the data to sessions
             session_name = "session_{}".format(session)
-            sessions[session_name] = {"run_1": raw}
+            sessions[session_name] = {}
+            if self.train_run:
+                sessions[session_name]['train'] = self._get_single_run(mat['EEG_MI_train'][0,0])
+            if self. test_run:
+                sessions[session_name]['test']  = self._get_single_run(mat['EEG_MI_test' ][0,0])
 
         return sessions
 
