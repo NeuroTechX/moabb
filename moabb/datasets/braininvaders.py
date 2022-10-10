921740b80c67dc948d3a435d94ad38a38f701cc9
gcattan <gregoire.cattan@ibm.com>
2022-10-10 21:57:19 +0200

    add utils function

--- moabb/datasets/braininvaders.py
+++ moabb/datasets/braininvaders.py
@@ -810,3 +810,21 @@ class VirtualReality(BaseDataset):
     def data_path(self, subject, path=None, force_update=False,
                   update_path=None, verbose=None):
         return _bi_data_path(self, subject, path, force_update, update_path, verbose)
+
+    def get_block_repetition(X, labels, meta, block_list, repetition_list):
+        # X, labels, meta = paradigm.get_data(dataset, subjects=[subject])
+        X_select = []
+        labels_select = []
+        meta_select = []
+        for block in block_list:
+            for repetition in repetition_list:
+                X_select.append(X[meta['run'] == 'block_' + str(block) + '-repetition_' + str(repetition)])
+                labels_select.append(labels[meta['run'] == 'block_' + str(block) + '-repetition_' + str(repetition)])
+                meta_select.append(meta[meta['run'] == 'block_' + str(block) + '-repetition_' + str(repetition)])
+        X_select = np.concatenate(X_select)
+        labels_select = np.concatenate(labels_select)
+        meta_select = np.concatenate(meta_select)
+        df = pd.DataFrame(meta_select, columns=meta.columns)
+        meta_select = df
+
+        return X_select, labels_select, meta_select
