3abc8dd2bf812c7311ed3be846e07e587506128c
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-01-26 00:14:44 +0100

    Ensuring that the repo is updated.

--- docker/create_docker.sh
+++ docker/create_docker.sh
@@ -3,8 +3,10 @@
 #
 # $ create_docker_image.sh
 set -ex
-TAG=moabb
+# Updating the repository or (cloning the repository and Navigate into the repository)
+git -C $TAG pull || true
 
+# Build the Docker image
 docker build . -f Dockerfile -t "${TAG}"  \
   --build-arg USER_ID=$(id -u) \
   --build-arg GROUP_ID=$(id -g) \
