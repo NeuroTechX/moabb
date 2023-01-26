0a0effc2fc294062a3328778047d2e3c009e621f
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-01-26 15:17:38 +0100

    Adding missed tag

--- docker/create_docker.sh
+++ docker/create_docker.sh
@@ -6,6 +6,8 @@ set -ex
 # Updating the repository or (cloning the repository and Navigate into the repository)
 git -C $TAG pull || true
 
+TAG=moabb
+
 # Build the Docker image
 docker build . -f Dockerfile -t "${TAG}"  \
   --build-arg USER_ID=$(id -u) \
