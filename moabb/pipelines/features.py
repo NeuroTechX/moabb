15972fdec09334ecfbd13225abbf3e52bc3a6295
CARRARA Igor <igor.carrara@inria.fr>
2023-02-13 10:42:29 +0100

    Adding pipeline Augmented Covariance Matrix

--- moabb/pipelines/features.py
+++ moabb/pipelines/features.py
@@ -61,3 +61,32 @@ class ExtendedSSVEPSignal(BaseEstimator, TransformerMixin):
         n_trials, n_freqs, n_channels, n_times = out.shape
         out = out.reshape((n_trials, n_channels * n_freqs, n_times))
         return out
+
+
+class AugmentedDataset(BaseEstimator, TransformerMixin):
+
+    def __init__(self, order=1, lag=1):
+
+        self.order = order
+        self.lag = lag
+
+    def fit(self, X, y):
+
+        return self
+
+    def transform(self, X):
+
+
+        if self.order == 1:
+            X_fin = X
+        else:
+            X_fin = []
+
+            for i in np.arange(X.shape[0]):
+                X_p = X[i][:, : -self.order*self.lag]
+                for p in np.arange(1, self.order):
+                    X_p = np.append(X_p, X[i][:, p*self.lag: -(self.order - p)*self.lag], axis=0)
+                X_fin.append(X_p)
+            X_fin = np.array(X_fin)
+
+        return X_fin
