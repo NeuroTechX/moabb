65ee3d8d54627bc4165e42ab9ef63792258ff8e7
Sylvain Chevallier <sylain.chevallier@universite-paris-saclay.fr>
2023-02-28 00:02:42 +0100

    add names and references

--- examples/plot_benchmark_DL.py
+++ examples/plot_benchmark_DL.py
@@ -40,7 +40,7 @@ GPU = len(tf.config.list_physical_devices("GPU")) > 0
 print("GPU is", "AVAILABLE" if GPU else "NOT AVAILABLE")
 
 ###############################################################################
-# In this example, we will use only the dataset 'BNCI2014001'.
+# In this example, we will use only the dataset ``BNCI2014001``.
 #
 # Running the benchmark
 # ---------------------
@@ -82,9 +82,51 @@ results = benchmark(
 )
 
 ###############################################################################
+# The deep learning architectures implemented in MOABB are:
+# - Shallow Convolutional Network [1]_
+# - Deep Convolutional Network [1]_
+# - EEGNet [2]_
+# - EEGTCNet [3]_
+# - EEGNex [4]_
+# - EEGITNet [5]_
+#
 # Benchmark prints a summary of the results. Detailed results are saved in a
 # pandas dataframe, and can be used to generate figures. The analysis & figures
 # are saved in the ``benchmark`` folder.
 
 score_plot(results)
 plt.show()
+
+##############################################################################
+# References
+# ----------
+# .. [1] `Functional connectivity ensemble method to enhance BCI performance
+#    (FUCONE)
+#    <https://arxiv.org/abs/2111.03122>`_
+#    Corsi, M.-C., Chevallier, S., De Vico Fallani, F. & Yger, F. IEEE TBME,
+#    2022
+# .. [1] Schirrmeister, R. T., Springenberg, J. T., Fiederer, L. D. J.,
+#    Glasstetter, M., Eggensperger, K., Tangermann, M., ... & Ball, T. (2017).
+#    `Deep learning with convolutional neural networks for EEG decoding and
+#    visualization <https://doi.org/10.1002/hbm.23730>`_.
+#    Human brain mapping, 38(11), 5391-5420.
+# .. [2] Lawhern, V. J., Solon, A. J., Waytowich, N. R., Gordon, S. M.,
+#    Hung, C. P., & Lance, B. J. (2018). `EEGNet: a compact convolutional neural
+#    network for EEG-based brain-computer interfaces.
+#    <https://doi.org/10.1088/1741-2552/aace8c>`_
+#    Journal of neural engineering, 15(5), 056013.
+# .. [3] Ingolfsson, T. M., Hersche, M., Wang, X., Kobayashi, N., Cavigelli, L., &
+#    Benini, L. (2020, October). `EEG-TCNet: An accurate temporal convolutional
+#    network for embedded motor-imagery brain-machine interfaces.
+#    <https://doi.org/10.1109/SMC42975.2020.9283028>`_
+#    In 2020 IEEE International Conference on Systems, Man, and Cybernetics (SMC)
+#    (pp. 2958-2965). IEEE.
+# .. [4] Chen, X., Teng, X., Chen, H., Pan, Y., & Geyer, P. (2022). `Toward reliable
+#    signals decoding for electroencephalogram: A benchmark study to EEGNeX.
+#    <https://doi.org/10.48550/arXiv.2207.12369>`_
+#    arXiv preprint arXiv:2207.12369.
+# .. [5] Salami, A., Andreu-Perez, J., & Gillmeister, H. (2022). `EEG-ITNet: An
+#    explainable inception temporal convolutional network for motor imagery
+#    classification
+#    <https://doi.org/10.1109/ACCESS.2022.3161489>`_.
+#    IEEE Access, 10, 36672-36685.
