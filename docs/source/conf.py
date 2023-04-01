db90806517f55a18ef10f60c1407054ac64b4cec
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-04-01 03:39:01 +0200

    fixing typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -160,7 +160,7 @@ sphinx_gallery_conf = {
     "show_memory": True,
     "reference_url": dict(moabb=None),
     "filename_pattern": "(/plot_|/tutorial_)",
-    "default_thumb_file": "../static/moabb_logo.svg",
+    "default_thumb_file": "../_static/moabb_logo.svg",
     "within_subsection_order": FileNameSortKey,
 }
 
