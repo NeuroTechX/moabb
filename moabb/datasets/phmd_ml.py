22ccf8afc4d5fb4b00e9768c8e9159609c5b7ba4
gcattan <gcattan@hotmail.fr>
2023-06-13 20:43:36 +0200

    add dataset

---
+++ moabb/datasets/phmd_ml.py
@@ -0,0 +1,104 @@
+import mne
+import numpy as np
+from . import download as dl
+from .base import BaseDataset
+from scipy.io import loadmat
+
+HEADMOUNTED_URL = 'https://zenodo.org/record/2617085/files/'
+
+class HeadMountedDisplay(BaseDataset):
+    '''
+    Passive Head Mounted Display with Music Listening dataset.
+
+    .. admonition:: Dataset summary
+
+
+        =================  =======  =======  ==========  =================  ============  ===============  ===========
+        Name                 #Subj    #Chan    #Classes    #Block / class    Trials len    Sampling rate    #Sessions
+        ================== =======  =======  ==========  =================  ============  ===============  ===========
+        HeadMountedDisplay      12       16           2                 10   60s            512Hz                   1
+        ================== =======  =======  ==========  =================  ============  ===============  ===========
+
+    We describe the experimental procedures for a dataset that we have made publicly available
+    at https://doi.org/10.5281/zenodo.2617084 in mat (Mathworks, Natick, USA) and csv formats.
+    This dataset contains electroencephalographic recordings of 12 subjects listening to music
+    with and without a passive head-mounted display, that is, a head-mounted display which does
+    not include any electronics at the exception of a smartphone. The electroencephalographic 
+    headset consisted of 16 electrodes. Data were recorded during a pilot experiment taking 
+    place in the GIPSA-lab, Grenoble, France, in 2017 (Cattan and al, 2018). 
+    The ID of this dataset is PHMDML.EEG.2017-GIPSA.
+   
+    **Full description of the experiment and dataset**
+    https://hal.archives-ouvertes.fr/hal-02085118
+    
+    **Link to the data**
+    https://doi.org/10.5281/zenodo.2617084
+ 
+    **Authors**
+    Principal Investigator: Eng. Grégoire Cattan
+    Technical Supervisors: Eng. Pedro L. C. Rodrigues
+    Scientific Supervisor: Dr. Marco Congedo
+    
+    **ID of the dataset**
+    PHMDML.EEG.2017-GIPSA
+
+    '''
+
+    def __init__(self):
+        super().__init__(
+            subjects=list(range(1, 12+1)),
+            sessions_per_subject=1,
+            events=dict(on=1, off=2),
+            code="PHMD-ML",
+            interval=[0, 1],
+            paradigm="rstate",
+            doi="https://doi.org/10.5281/zenodo.2617084 "
+        )
+
+    def _get_single_subject_data(self, subject):
+        """return data for a single subject"""
+
+        filepath = self.data_path(subject)[0]
+        data = loadmat(filepath)
+
+        S = data['data'][:, 1:17]
+        stim = data['data'][:, -1]
+
+        chnames = [
+            'Fp1',
+            'Fp2',
+            'Fc5',
+            'Fz',
+            'Fc6',
+            'T7',
+            'Cz',
+            'T8',
+            'P7',
+            'P3',
+            'Pz',
+            'P4',
+            'P8',
+            'O1',
+            'Oz',
+            'O2',
+            'stim']
+        chtypes = ['eeg'] * 16 + ['stim']
+        X = np.concatenate([S, stim[:, None]], axis=1).T
+
+        info = mne.create_info(ch_names=chnames, sfreq=512,
+                               ch_types=chtypes,
+                               verbose=False)
+        raw = mne.io.RawArray(data=X, info=info, verbose=False)
+
+        return raw
+
+    def data_path(self, subject, path=None, force_update=False,
+                  update_path=None, verbose=None):
+
+        if subject not in self.subject_list:
+            raise(ValueError("Invalid subject number"))
+
+        url = '{:s}subject_{:02d}.mat'.format(HEADMOUNTED_URL, subject)
+        file_path = dl.data_path(url, 'HEADMOUNTED')
+
+        return [file_path]
--- moabb/paradigms/resting_state.py
+++ moabb/paradigms/resting_state.py
@@ -1,10 +1,45 @@
 from moabb.paradigms.p300 import SinglePass
 
 class RestingStateToP300Adapter(SinglePass):
-    """P300 for Target/NonTarget classification
+    """Adapter to the P300 paradigm for resting state experiments.
 
-    Metric is 'roc_auc'
+    Parameters
+    ----------
+    fmin: float (default 10)
+        cutoff frequency (Hz) for the high pass filter
 
+    fmax: float (default 50)
+        cutoff frequency (Hz) for the low pass filter
+
+    events: List of str | None (default None)
+        event to use for epoching. If None, default to all events defined in
+        the dataset.
+
+    tmin: float (default 1.0)
+        Start time (in second) of the epoch, relative to the dataset specific
+        task interval e.g. tmin = 1 would mean the epoch will start 1 second
+        after the beginning of the task as defined by the dataset.
+
+    tmax: float | None, (default 35)
+        End time (in second) of the epoch, relative to the beginning of the
+        dataset specific task interval. tmax = 5 would mean the epoch will end
+        5 second after the beginning of the task as defined in the dataset. If
+        None, use the dataset value.
+
+    resample: float | None (default 128)
+        If not None, resample the eeg data with the sampling rate provided.
+
+    baseline: None | tuple of length 2
+            The time interval to consider as “baseline” when applying baseline
+            correction. If None, do not apply baseline correction.
+            If a tuple (a, b), the interval is between a and b (in seconds),
+            including the endpoints.
+            Correction is applied by computing the mean of the baseline period
+            and subtracting it from the data (see mne.Epochs)
+
+    channels: list of str | None (default None)
+        list of channel to select. If None, use all EEG channels available in
+        the dataset.
     """
 
     def __init__(self, fmin=10, fmax=50, tmin=1, tmax=35, resample=128, **kwargs):
@@ -12,6 +47,18 @@ class RestingStateToP300Adapter(SinglePass):
 
     def used_events(self, dataset):
         return {ev: dataset.event_id[ev] for ev in self.events}
+
+    def is_valid(self, dataset):
+        ret = True
+        if not (dataset.paradigm == "rstate"):
+            ret = False
+
+        if self.events:
+            if not set(self.events) <= set(dataset.event_id.keys()):
+                ret = False
+
+        # we should verify list of channels, somehow
+        return ret
 
     @property
     def scoring(self):
