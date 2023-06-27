8bcbb06659b8071ce038cc06355a0c6ea341862f
Sara04 <sara.sedlar@gmail.com>
2023-06-27 02:03:31 +0200

    Update dataset and add example

---
+++ examples/plot_Dreyer_clf_scores_vs_subj_info.py
@@ -0,0 +1,55 @@
+import moabb
+from moabb.datasets import Dreyer2023A, Dreyer2023B, Dreyer2023C
+
+import moabb
+from moabb.datasets import Dreyer2023A
+from moabb.evaluations import WithinSessionEvaluation
+from moabb.paradigms import MotorImagery
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+from pyriemann.estimation import Covariances
+from pyriemann.spatialfilters import CSP
+from sklearn.pipeline import make_pipeline
+from mne.decoding import Vectorizer
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+dreyer2023 = Dreyer2023A()
+paradigm = MotorImagery()
+pipelines = {}
+pipelines['CSP+LDA'] = make_pipeline(Covariances(estimator="oas"), 
+                                     CSP(nfilter=6), 
+                                     LDA(solver="lsqr", shrinkage="auto"))
+  
+evaluation = WithinSessionEvaluation(paradigm=paradigm, datasets=[dreyer2023], 
+                                     suffix="examples", overwrite=False)
+results = evaluation.process(pipelines)
+
+
+##############################################################################
+info = dreyer2023.get_subject_info(infos=['Demo_Bio'])
+results_info = pd.concat([info, results], axis=1)
+fig, ax = plt.subplots(facecolor="white", figsize=[8, 4])
+plt.figure(1)
+sb.stripplot(
+    data=results_info,
+    y="score",
+    x="SUJ_gender",
+    ax=ax,
+    jitter=True,
+    alpha=0.5,
+    zorder=1,
+    palette="Set1",
+)
+
+sb.pointplot(data=results_info, y="score", x="SUJ_gender", ax=ax, palette="Set1")
+ax.set_xticklabels(['Man', 'Woman'])
+ax.set_ylabel("ROC AUC")
+ax.set_ylim(0.5, 1)
+
+fig, ax2 = plt.subplots(facecolor="white", figsize=[8, 4])
+
+sb.regplot(data=results_info[['score', 'Birth_year']].astype('float32'), 
+           y="score", x="Birth_year", ax=ax2, 
+           scatter_kws = {"color": "black", "alpha": 0.5}, line_kws = {"color": "red"},)
+plt.show()
--- moabb/datasets/Dreyer2023.py
+++ moabb/datasets/Dreyer2023.py
@@ -11,6 +11,7 @@ import zipfile
 from functools import partialmethod
 from os.path import dirname, join
 from pathlib import Path
+import pandas as pd
 
 import mne
 from mne.channels import make_standard_montage
@@ -25,6 +26,29 @@ from .download import get_dataset_path
 
 DREYER2023_URL = "https://zenodo.org/record/7554429/files/BCI Database.zip"
 
+SUBJ_INFO = \
+{"Demo_Bio":               ["SUJ_gender", "Birth_year", "Vision", "Vision_assistance",
+                            "Symptoms_TXT", "Level of study", "Level_knowledge neuro",
+                            "Meditation practice", "Laterality answered", 
+                            "Manual activity", "Manual activity TXT"],
+ "OpenVibe_Perf":          ["Perf_RUN_3", "Perf_RUN_4", "Perf_RUN_5", "Perf_RUN_6"],
+ "Mental_Rotation":        ["score", "time_1", "time_2"],
+ "PRE_session":            ["PRE_Mood", "PRE_Mindfulness", "PRE_Motivation", 
+                            "PRE_Hours_sleep_last_night", "PRE_Usual_sleep", 
+                            "PRE_Level_of_alertness", "PRE_Stimulant_doses_12h", 
+                            "PRE_Stimulant_doses_2h", "PRE_Stim_normal", "PRE_Tabacco", 
+                            "PRE_Tabacco_normal", "PRE_Alcohol", "PRE_Last_meal", 
+                            "PRE_Last_pills", "PRE_Pills_TXT", "PRE_Nervousness", 
+                            "PRE_Awakening", "PRE_Concentration"],
+ "POST_session":           ["POST_Mood", "POST_Mindfulness", "POST_Motivation", 
+                            "POST_Cognitive load", "POST_Agentivity", 
+                            "POST_Expectations_filled"],
+ "Index_of_Learnig_Style": ["active", "reflexive", "sensory", "intuitive", "visual",
+                           "verbal", "sequential", "global"],
+ "16Pf5":                  ["A", "B", "C_", "E", "F", "G", "H", "I", "L", "M", "N", "O",
+                           "Q1", "Q2", "Q3", "Q4", "IM", "EX", "AX", "TM", "IN", "SC",
+                           "Interrogation"]
+}
 
 class Dreyer2023(BaseDataset):
     """Class for Dreyer2023 dataset management. MI dataset.
@@ -34,7 +58,7 @@ class Dreyer2023(BaseDataset):
         ==========  =======  =======  ==========  =================  ============  ===============  ===========
         Name         #Subj    #Chan    #Classes    #Trials / class    Trials len    Sampling rate    #Sessions
         ==========  =======  =======  ==========  =================  ============  ===============  ===========
-        Dreyer2023    87       27         2                                            512 Hz           6
+        Dreyer2023    87       27         2               20              5s            512 Hz           6
         ==========  =======  =======  ==========  =================  ============  ===============  ===========
 
     ===================
@@ -56,14 +80,55 @@ class Dreyer2023(BaseDataset):
                "Invalid dataset selection! Existing Dreyer2023 datasets: A, B, and C."
         self.db_id = db_id
         self.db_idx_off = dict(A=0, B=60, C=81)
+
         super().__init__(subjects, sessions_per_subject=1,
                          events=dict(left_hand=1, right_hand=2),
                          code='Dreyer2023',  interval=[3, 8], paradigm='imagery',
                          doi='10.5281/zenodo.7554429')
 
+    def get_subject_info(self, subjects=[], infos=['Demo_Bio']):
+
+        path = get_dataset_path("DREYER", path=None)
+        dlpath = dl.data_dl(DREYER2023_URL, "DREYER_2023", path)
+        perform_path = join(dirname(dlpath), 'BCI Database', 'Perfomances.xlsx')
+
+        df = pd.read_excel(perform_path)
+
+        if self.db_id == 'A':
+            df.columns = df.iloc[1, :]
+            df = df.iloc[list(range(2,62)), :]
+        if self.db_id == 'B':
+            df.columns = df.iloc[65]
+            df = df.iloc[list(range(66,87)), :]
+        if self.db_id == 'C':
+            df.columns = df.iloc[90]
+            df = df.iloc[list(range(91,97)), :]
+        df.reset_index(drop=True, inplace=True)
+        df.columns.name = None
+        if len(subjects):
+            subjects = [self.db_id + str(s) 
+                        if not str(s).startswith(self.db_id) else str(s)
+                        for s in subjects]
+            assert not any([s for s in subjects if s not in df['SUJ_ID'].tolist()]),\
+                "Invalid subject selection."
+            df = df.loc[df['SUJ_ID'].isin(subjects)]
+
+        info_select = ['SUJ_ID']
+        for i in infos:
+            if i in SUBJ_INFO.keys():
+                for j in SUBJ_INFO[i]:
+                    if j in df.columns:
+                        info_select.append(j)
+            elif i in df.columns:
+                info_select.append(i)
+            else:
+                raise "Ivalid info selection."
+        return df[info_select]
+
     def _get_single_subject_data(self, subject):
 
         subj_dir = self.data_path(subject)
+
         subj_id = self.db_id + str(subject + self.db_idx_off[self.db_id])
 
         ch_names = ['Fz', 'FCz', 'Cz', 'CPz', 'Pz', 'C1', 'C3', 'C5',
@@ -96,7 +161,11 @@ class Dreyer2023(BaseDataset):
 
             recordings['run_%d' % r_i] = \
                 read_raw_gdf(join(subj_dir, subj_id + '_{0}_{1}.gdf'.format(r_n, r_t)),
+                             preload=True,
                              eog=['EOG1', 'EOG2', 'EOG3'], misc=['EMGg', 'EMGd'])
+
+            recordings['run_%d' % r_i].annotations.rename({'769':'left_hand', 
+                                                           '770':'right_hand'})
 
         return {"session_0": recordings}
 
@@ -112,6 +181,8 @@ class Dreyer2023(BaseDataset):
 
         dlpath = dl.data_dl(DREYER2023_URL, "DREYER_2023", path)
         if not os.path.exists(join(dirname(dlpath), 'BCI Database')):
+           with zipfile.ZipFile(dlpath) as zip_file:
+                zip_file.extractall(dirname(dlpath))
 
         subj_temp_path = join(dirname(dlpath), 'BCI Database', 'Signals/DATA {0}/{0}{1}')
         return subj_temp_path.format(self.db_id, subject + self.db_idx_off[self.db_id])
