2e443f3cff624a2281050994a969de422dfcd16b
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2023-07-31 19:45:18 +0200

    Updating the tutorial with my review

--- examples/plot_bids_conversion.py
+++ examples/plot_bids_conversion.py
@@ -4,16 +4,40 @@ Convert a MOABB dataset to BIDS
 ===============================
 
 The Brain Imaging Data Structure (BIDS) format
-is a standard format for storing neuroimaging data.
+is standard for storing neuroimaging data.
 It follows fixed principles to facilitate the
 sharing of neuroimaging data between researchers.
 
-The MOABB library allows to convert any dataset to
-BIDS. This example shows how this can be done.
+The MOABB library allows to convert any MOABB dataset to
+BIDS [1]_ and [2]_.
 
-We will use the AlexMI dataset as example
-because it is relatively small and can be
-downloaded quickly.
+In this example, we will convert the AlexMI dataset to BIDS using the
+option `cache_config=dict(path=temp_dir, save_raw=True)` of the get_data
+method from the dataset object.
+
+This will automatically save the raw data in the BIDS format and allow to use
+a cache for the next time the dataset is used.
+
+We will use the AlexMI dataset [3]_, one of the smallest in
+people and one that can be downloaded quickly.
+
+References
+-----------
+
+.. [1] Pernet, C.R., Appelhoff, S., Gorgolewski, K.J. et al. EEG-BIDS,
+       An extension to the brain imaging data structure for
+       electroencephalography. Sci Data 6, 103 (2019).
+       https://doi.org/10.1038/s41597-019-0104-8
+
+.. [2] Appelhoff et al., (2019). MNE-BIDS: Organizing electrophysiological
+       data into the BIDS format and facilitating their analysis.
+       Journal of Open Source Software, 4(44), 1896,
+       https://doi.org/10.21105/joss.01896
+
+.. [3] Barachant, A., 2012. Commande robuste d'un effecteur par une
+       interface cerveau machine EEG asynchrone (Doctoral dissertation,
+       Université de Grenoble).
+       https://tel.archives-ouvertes.fr/tel-01196752
 
 """
 # Authors: Pierre Guetschel <pierre.guetschel@gmail.com>
@@ -36,10 +60,11 @@ set_log_level("info")
 # Basic usage
 # -----------
 #
-# The conversion of any MOABB dataset to a BIDS-compliant structure can be done
-# by simply calling its ``get_data`` method. Here we will save the BIDS version
-# of the dataset in a temporary folder:
+# Here, we will save the BIDS version of the dataset in a temporary folder
 temp_dir = Path(tempfile.mkdtemp())
+# The conversion of any MOABB dataset to a BIDS-compliant structure can be done
+# by simply calling its ``get_data`` method and using the ``cache_config``
+# parameter. This parameter is a dictionary.
 dataset = AlexMI()
 _ = dataset.get_data(cache_config=dict(path=temp_dir, save_raw=True))
 
@@ -76,7 +101,7 @@ print("Before conversion:")
 print_tree(mne_data / "MNE-alexeeg-data")
 
 ###############################################################################
-# As we can see, before conversion all the data (i.e. from all subjects,
+# As we can see, before conversion, all the data (i.e. from all subjects,
 # sessions and runs) is stored in a single folder. This follows no particular
 # standard and can vary from one dataset to another.
 #
@@ -87,16 +112,18 @@ print_tree(temp_dir / "MNE-alexandre motor imagery-bids-cache")
 ###############################################################################
 # In the BIDS version of our dataset, the raw files are saved in EDF.
 # The data is organized in a hierarchy of folders,
-# starting with the subjects, then the sessions, then the runs. Metadata files
-# are stored to describe the data. For more details on the BIDS structure,
-# please refer to the `BIDS website <https://bids.neuroimaging.io>`_ and the
-# `BIDS specification <https://bids-specification.readthedocs.io/en/stable/>`_.
+# starting with the subjects, then the sessions, and then the runs. Metadata
+# files are stored to describe the data. For more details on the BIDS
+# structure, please refer to the `BIDS website <https://bids.neuroimaging.io>`_
+# and the `BIDS spec <https://bids-specification.readthedocs.io/en/stable/>`_.
 #
 # Under the hood, saving datasets to BIDS is done through the caching system
-# of MOABB. Only raw EEG files are officially supported by the BIDS specification.
-# However, MOABB's caching mechanism also offers the possibility to save the data
-# in a pseudo-BIDS after different preprocessing steps. In particular, we can save
-# :class:`mne.Epochs` and ``np.ndarray`` objects.  For more details on the caching system,
+# of MOABB. Only raw EEG files are officially supported by the BIDS
+# specification.
+# However, MOABB's caching mechanism also offers the possibility to save
+# the data in a pseudo-BIDS after different preprocessing steps.
+# In particular, we can save :class:`mne.Epochs` and ``np.ndarray`` objects.
+# For more details on the caching system,
 # please refer to the tutorial :doc:`./plot_disk_cache`.
 #
 # Cleanup
