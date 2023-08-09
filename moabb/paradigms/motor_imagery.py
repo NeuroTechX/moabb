1a4b71be2ae9e57acaa5be0602a8fd3bbea8534a
Sara Sedlar <ssedlar@margaret.saclay.inria.fr>
2023-08-10 00:25:04 +0200

    Remove repeating

--- moabb/paradigms/motor_imagery.py
+++ moabb/paradigms/motor_imagery.py
@@ -105,18 +105,19 @@ class BaseMotorImagery(BaseParadigm):
         return "accuracy"
 
 
-class SinglePass(BaseMotorImagery):
-    """Single Bandpass filter motor Imagery.
+class BandPass(BaseMotorImagery):
+    """Filter Motor Imagery signals.
 
-    Motor imagery paradigm with only one bandpass filter (default 8 to 32 Hz)
+    Motor imagery paradigm with one bandpass filter (default 8 to 32 Hz) 
+    or with a filter bank.
 
     Parameters
     ----------
     fmin: float (default 8)
-        cutoff frequency (Hz) for the high pass filter
+        cutoff frequency (Hz) for the high pass filter in single band pass filter
 
     fmax: float (default 32)
-        cutoff frequency (Hz) for the low pass filter
+        cutoff frequency (Hz) for the low pass filter in single band pass filter
 
     events: List of str | None (default None)
         event to use for epoching. If None, default to all events defined in
@@ -149,25 +150,16 @@ class SinglePass(BaseMotorImagery):
         If not None, resample the eeg data with the sampling rate provided.
     """
 
-    def __init__(self, fmin=8, fmax=32, **kwargs):
-        if "filters" in kwargs.keys():
-            raise (ValueError("MotorImagery does not take argument filters"))
-        super().__init__(filters=[[fmin, fmax]], **kwargs)
+    def __init__(self, filter_type="single_pass", fmin=8, fmax=32, **kwargs):
+        if filter_type == "single_pass":
+            if "filters" in kwargs.keys():
+                raise (ValueError("MotorImagery does not take argument filters"))
+            super().__init__(filters=[[fmin, fmax]], **kwargs)
+        elif filter_type == "filter_bank":
+            super().__init__(**kwargs)
 
 
-class FilterBank(BaseMotorImagery):
-    """Filter Bank MI."""
-
-    def __init__(
-        self,
-        filters=([8, 12], [12, 16], [16, 20], [20, 24], [24, 28], [28, 32]),
-        **kwargs,
-    ):
-        """init."""
-        super().__init__(filters=filters, **kwargs)
-
-
-class LeftRightImagery(SinglePass):
+class LeftRightImagery(BandPass):
     """Motor Imagery for left hand/right hand classification.
 
     Metric is 'roc_auc'
@@ -186,111 +178,17 @@ class LeftRightImagery(SinglePass):
         return "roc_auc"
 
 
-class FilterBankLeftRightImagery(FilterBank):
+class FilterBankLeftRightImagery(LeftRightImagery):
     """Filter Bank Motor Imagery for left hand/right hand classification.
 
     Metric is 'roc_auc'
     """
 
-    def __init__(self, **kwargs):
-        if "events" in kwargs.keys():
-            raise (ValueError("LeftRightImagery dont accept events"))
-        super().__init__(events=["left_hand", "right_hand"], **kwargs)
-
-    def used_events(self, dataset):
-        return {ev: dataset.event_id[ev] for ev in self.events}
-
-    @property
-    def scoring(self):
-        return "roc_auc"
+    def __init__(self, filters=([8, 12], [12, 16], [16, 20], [20, 24], [24, 28], [28, 32]), **kwargs):
+        super().__init__(filter_type="filter_bank", filters=filters, **kwargs)
 
 
-class FilterBankMotorImagery(FilterBank):
-    """Filter bank n-class motor imagery.
-
-    Metric is 'roc-auc' if 2 classes and 'accuracy' if more
-
-    Parameters
-    -----------
-
-    events: List of str
-        event labels used to filter datasets (e.g. if only motor imagery is
-        desired).
-
-    n_classes: int,
-        number of classes each dataset must have. If events is given,
-        requires all imagery sorts to be within the events list.
-    """
-
-    def __init__(self, n_classes=None, **kwargs):
-        "docstring"
-        super().__init__(**kwargs)
-        self.n_classes = n_classes
-
-        if self.events is None:
-            log.warning("Choosing from all possible events")
-        elif self.n_classes is not None:
-            assert n_classes <= len(self.events), "More classes than events specified"
-
-    def is_valid(self, dataset):
-        ret = True
-        if not dataset.paradigm == "imagery":
-            ret = False
-        elif self.n_classes is None and self.events is None:
-            pass
-        elif self.events is None:
-            if len(dataset.event_id) < self.n_classes:
-                ret = False
-        else:
-            overlap = len(set(self.events) & set(dataset.event_id.keys()))
-            if not overlap >= self.n_classes:
-                ret = False
-        return ret
-
-    def used_events(self, dataset):
-        out = {}
-        if self.events is None:
-            for k, v in dataset.event_id.items():
-                out[k] = v
-            if self.n_classes is None:
-                self.n_classes = len(out)
-        else:
-            for event in self.events:
-                if event in dataset.event_id.keys():
-                    out[event] = dataset.event_id[event]
-                if len(out) == self.n_classes:
-                    break
-        if len(out) < self.n_classes:
-            raise (
-                ValueError(
-                    f"Dataset {dataset.code} did not have enough "
-                    f"events in {self.events} to run analysis"
-                )
-            )
-        return out
-
-    @property
-    def datasets(self):
-        if self.tmax is None:
-            interval = None
-        else:
-            interval = self.tmax - self.tmin
-        return utils.dataset_search(
-            paradigm="imagery",
-            events=self.events,
-            interval=interval,
-            has_all_events=False,
-        )
-
-    @property
-    def scoring(self):
-        if self.n_classes == 2:
-            return "roc_auc"
-        else:
-            return "accuracy"
-
-
-class MotorImagery(SinglePass):
+class MotorImagery(BandPass):
     """N-class motor imagery.
 
     Metric is 'roc-auc' if 2 classes and 'accuracy' if more
@@ -405,6 +303,13 @@ class MotorImagery(SinglePass):
         else:
             return "accuracy"
 
+class FilterBankMotorImagery(MotorImagery):
+    """Filter bank n-class motor imagery.
+
+    Metric is 'roc-auc' if 2 classes and 'accuracy' if more
+    """
+    def __init__(self, filters=([8, 12], [12, 16], [16, 20], [20, 24], [24, 28], [28, 32]), **kwargs):
+        super().__init__(filter_type="filter_bank", filters=filters, **kwargs)
 
 class FakeImageryParadigm(LeftRightImagery):
     """Fake Imagery for left hand/right hand classification."""
