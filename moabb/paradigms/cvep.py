5ff57bf5248068a136cea6a7c05de51076ce24f8
Jordy Thielen <jordy.thielen@donders.ru.nl>
2023-08-17 15:26:33 +0200

    add cvep paradigm

---
+++ moabb/paradigms/cvep.py
@@ -0,0 +1,354 @@
+"""v-VEP Paradigms"""
+
+import abc
+import logging
+
+import mne
+import numpy as np
+import pandas as pd
+
+from moabb.datasets import utils
+from moabb.datasets.fake import FakeDataset
+from moabb.paradigms.base import BaseParadigm
+
+
+log = logging.getLogger(__name__)
+
+
+class BaseCVEP(BaseParadigm):
+    """Base c-VEP paradigm.
+
+    Please use one of the child classes.
+
+    Parameters
+    ----------
+
+    filters: list of list (defaults [[1, 40]])
+        Bank of bandpass filter to apply.
+
+    events: List of str | None (default None)
+        Event to use for epoching. If None, default to all events defined in
+        the dataset.
+
+    tmin: float (default 0.0)
+        Start time (in second) of the epoch, relative to the dataset specific
+        task interval e.g. tmin = 1 would mean the epoch will start 1 second
+        after the beginning of the task as defined by the dataset.
+
+    tmax: float | None (default None)
+        End time (in second) of the epoch, relative to the beginning of the
+        dataset specific task interval. tmax = 5 would mean the epoch will end
+        5 second after the beginning of the task as defined in the dataset. If
+        None, use the dataset value.
+
+    baseline: None | tuple of length 2 (default None)
+        The time interval to consider as “baseline” when applying baseline
+        correction. If None, do not apply baseline correction.
+        If a tuple (a, b), the interval is between a and b (in seconds),
+        including the endpoints.
+        Correction is applied by computing the mean of the baseline period
+        and subtracting it from the data (see mne.Epochs)
+
+    channels: list of str | None (default None)
+        list of channel to select. If None, use all EEG channels available in
+        the dataset.
+
+    resample: float | None (default None)
+        If not None, resample the eeg data with the sampling rate provided.
+    """
+
+    def __init__(
+        self,
+        filters=([1, 40],),
+        events=None,
+        tmin=0.0,
+        tmax=None,
+        baseline=None,
+        channels=None,
+        resample=None,
+    ):
+        super().__init__()
+        self.filters = filters
+        self.events = events
+        self.baseline = baseline
+        self.channels = channels
+        self.resample = resample
+
+        if tmax is not None:
+            if tmin >= tmax:
+                raise (ValueError("tmax must be greater than tmin"))
+
+        self.tmin = tmin
+        self.tmax = tmax
+
+    def is_valid(self, dataset):
+        ret = True
+        if not (dataset.paradigm == "cvep"):
+            ret = False
+
+        # check if dataset has required events
+        if self.events:
+            if not set(self.events) <= set(dataset.event_id.keys()):
+                ret = False
+
+        return ret
+
+    @abc.abstractmethod
+    def used_events(self, dataset):
+        pass
+
+    def process_raw(
+        self, raw, dataset, return_epochs=False, return_raws=False
+    ):
+        """
+        Process one raw data file.
+
+        This function applies the preprocessing and eventual epoching on the
+        individual run, and return the data, labels and a dataframe with
+        metadata.
+
+        metadata is a dataframe with as many row as the length of the data
+        and labels.
+
+        Parameters
+        ----------
+        raw: mne.Raw instance
+            the raw EEG data.
+        dataset : dataset instance
+            The dataset corresponding to the raw file. Mainly used to access
+            dataset specific information.
+        return_epochs: boolean
+            This flag specifies whether to return only the data array or the
+            complete processed mne.Epochs
+        return_raws: boolean
+            To return raw files and events, to ensure compatibility with braindecode.
+            Mutually exclusive with return_epochs
+
+        returns
+        -------
+        X : Union[np.ndarray, mne.Epochs]
+            the data that will be used as features for the model
+            Note: if return_epochs=True,  this is mne.Epochs
+            if return_epochs=False, this is np.ndarray
+        labels: np.ndarray
+            the labels for training / evaluating the model
+        metadata: pd.DataFrame
+            A dataframe containing the metadata
+
+        """
+
+        if return_epochs and return_raws:
+            message = "Select only return_epochs or return_raws, not both"
+            raise ValueError(message)
+
+        # get events id
+        event_id = self.used_events(dataset)
+
+        # find the events, first check stim_channels then annotations
+        stim_channels = mne.utils._get_stim_channel(None, raw.info, raise_error=False)
+        if len(stim_channels) > 0:
+            events = mne.find_events(raw, shortest_event=0, verbose=False)
+        else:
+            log.warning(f"No matching stim channels found in {raw.filenames}")
+
+        # picks channels
+        if self.channels is None:
+            picks = mne.pick_types(raw.info, eeg=True, stim=False)
+        else:
+            picks = mne.pick_channels(
+                raw.info["ch_names"], include=self.channels, ordered=True
+            )
+
+        # pick events, based on event_id
+        try:
+            if "Target" in event_id and "NonTarget" in event_id:
+                if (
+                    isinstance(event_id["Target"], list)
+                    and isinstance(event_id["NonTarget"], list)
+                ):
+                    event_id_new = dict(Target=1, NonTarget=0)
+                    events = mne.merge_events(events, event_id["Target"], 1)
+                    events = mne.merge_events(events, event_id["NonTarget"], 0)
+                    event_id = event_id_new
+            events = mne.pick_events(events, include=list(event_id.values()))
+        except RuntimeError:
+            # skip raw if no event found
+            return
+
+        if return_raws:
+            raw = raw.pick(picks)
+        else:
+            # get interval
+            tmin = self.tmin + dataset.interval[0]
+            if self.tmax is None:
+                tmax = dataset.interval[1]
+            else:
+                tmax = self.tmax + dataset.interval[0]
+
+            X = []
+            for bandpass in self.filters:
+                fmin, fmax = bandpass
+                # filter data
+                raw_f = raw.copy().filter(
+                    fmin, fmax, method="iir", picks=picks, verbose=False
+                )
+                # epoch data
+                baseline = self.baseline
+                if baseline is not None:
+                    baseline = (
+                        self.baseline[0] + dataset.interval[0],
+                        self.baseline[1] + dataset.interval[0],
+                    )
+                    bmin = baseline[0] if baseline[0] < tmin else tmin
+                    bmax = baseline[1] if baseline[1] > tmax else tmax
+                else:
+                    bmin = tmin
+                    bmax = tmax
+                epochs = mne.Epochs(
+                    raw_f,
+                    events,
+                    event_id=event_id,
+                    tmin=bmin,
+                    tmax=bmax,
+                    proj=False,
+                    baseline=baseline,
+                    preload=True,
+                    verbose=False,
+                    picks=picks,
+                    event_repeated="drop",
+                    on_missing="ignore",
+                )
+                if bmin < tmin or bmax > tmax:
+                    epochs.crop(tmin=tmin, tmax=tmax)
+                if self.resample is not None:
+                    epochs = epochs.resample(self.resample)
+                # rescale to work with uV
+                if return_epochs:
+                    X.append(epochs)
+                else:
+                    X.append(dataset.unit_factor * epochs.get_data())
+
+            # overwrite events in case epochs have been dropped:
+            # (assuming all filters produce the same number of epochs...)
+            events = epochs.events
+
+        inv_events = {k: v for v, k in event_id.items()}
+        labels = np.array([inv_events[e] for e in events[:, -1]])
+
+        if return_epochs:
+            X = mne.concatenate_epochs(X)
+        elif return_raws:
+            X = raw
+        elif len(self.filters) == 1:
+            # if only one band, return a 3D array
+            X = X[0]
+        else:
+            # otherwise return a 4D
+            X = np.array(X).transpose((1, 2, 3, 0))
+
+        metadata = pd.DataFrame(index=range(len(labels)))
+        return X, labels, metadata
+
+    @property
+    def datasets(self):
+        if self.tmax is None:
+            interval = None
+        else:
+            interval = self.tmax - self.tmin
+        return utils.dataset_search(
+            paradigm="cvep", events=self.events, interval=interval, has_all_events=True
+        )
+
+    @property
+    def scoring(self):
+        return "roc_auc"
+
+
+class SinglePass(BaseCVEP):
+    """Single Bandpass filter c-VEP
+
+    c-VEP paradigm with only one bandpass filter (default 1 to 40 Hz)
+
+    Parameters
+    ----------
+    fmin: float (default 1)
+        cutoff frequency (Hz) for the high pass filter
+
+    fmax: float (default 40)
+        cutoff frequency (Hz) for the low pass filter
+
+    events: List of str | None (default None)
+        event to use for epoching. If None, default to all events defined in
+        the dataset.
+
+    tmin: float (default 0.0)
+        Start time (in second) of the epoch, relative to the dataset specific
+        task interval e.g. tmin = 1 would mean the epoch will start 1 second
+        after the beginning of the task as defined by the dataset.
+
+    tmax: float | None (default None)
+        End time (in second) of the epoch, relative to the beginning of the
+        dataset specific task interval. tmax = 5 would mean the epoch will end
+        5 second after the beginning of the task as defined in the dataset. If
+        None, use the dataset value.
+
+    baseline: None | tuple of length 2 (default None)
+            The time interval to consider as “baseline” when applying baseline
+            correction. If None, do not apply baseline correction.
+            If a tuple (a, b), the interval is between a and b (in seconds),
+            including the endpoints.
+            Correction is applied by computing the mean of the baseline period
+            and subtracting it from the data (see mne.Epochs)
+
+    channels: list of str | None (default None)
+        list of channel to select. If None, use all EEG channels available in
+        the dataset.
+
+    resample: float | None (default None)
+        If not None, resample the eeg data with the sampling rate provided.
+
+    """
+
+    def __init__(self, fmin=1, fmax=40, **kwargs):
+        if "filters" in kwargs.keys():
+            raise (ValueError("c-VEP does not take argument filters"))
+        super().__init__(filters=[[fmin, fmax]], **kwargs)
+
+    @property
+    def fmax(self):
+        return self.filters[0][1]
+
+    @property
+    def fmin(self):
+        return self.filters[0][0]
+
+
+class CVEP(SinglePass):
+    """c-VEP for Target/NonTarget classification
+
+    Metric is 'roc_auc'
+
+    """
+
+    def __init__(self, **kwargs):
+        if "events" in kwargs.keys():
+            raise (ValueError("c-VEP does not accept events"))
+        super().__init__(events=["Target", "NonTarget"], **kwargs)
+
+    def used_events(self, dataset):
+        return {ev: dataset.event_id[ev] for ev in self.events}
+
+    @property
+    def scoring(self):
+        return "roc_auc"
+
+
+class FakeCVEPParadigm(CVEP):
+    """Fake c-VEP for Target/NonTarget classification."""
+
+    @property
+    def datasets(self):
+        return [FakeDataset(["Target", "NonTarget"], paradigm="cvep")]
+
+    def is_valid(self, dataset):
+        return True
