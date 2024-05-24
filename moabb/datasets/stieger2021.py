a1f6f6a42aadb9a23ca17aa2cd162ad59e6ef502
Reinmar Kobler <reinmar.kobler@atr.jp>
2024-05-24 15:19:19 +0900

    Drop constraint on interval and logging formatting

--- moabb/datasets/stieger2021.py
+++ moabb/datasets/stieger2021.py
@@ -35,14 +35,14 @@ class Stieger2021(BaseDataset):
 
     The main goals of our original study were to characterize how individuals
     learn to control SMR-BCIs and to test whether this learning can be improved
-    through behavioral interventions such as mindfulness training30. Participants
+    through behavioral interventions such as mindfulness training. Participants
     were initially assessed for baseline BCI proficiency and then randomly
     assigned to an 8-week mindfulness intervention (Mindfulness-based stress
     reduction), or waitlist control condition where participants waited for
     the same duration as the MBSR class before starting BCI training, but
     were offered a comparable MBSR course after completing all experimental
-    requirements46. Following the 8-weeks, participants returned to the lab
-    for 6–10 sessions of BCI training.
+    requirements. Following the 8-weeks, participants returned to the lab
+    for 6 to 10 sessions of BCI training.
 
     All experiments were approved by the institutional review boards of the
     University of Minnesota and Carnegie Mellon University. Informed consents
@@ -58,9 +58,9 @@ class Stieger2021(BaseDataset):
     those that did not demonstrate ceiling performance in the baseline BCI
     assessment (accuracy above 90% in 1D control). The dataset descriptor
     presented here describes data collected from 62 participants:
-    33 MBSR participants (Age=42+/−15, (F)emale=26) and 29 controls
-    (Age=36+/−13, F=23). In the United States, women are twice as likely to
-    practice meditation compared to men47,48. Therefore, the gender
+    33 MBSR participants (Age=42+/-15, (F)emale=26) and 29 controls
+    (Age=36+/-13, F=23). In the United States, women are twice as likely to
+    practice meditation compared to men. Therefore, the gender
     imbalance in our study may result from a greater likelihood of
     women to respond to flyers offering a meditation class in exchange
     for participating in our study.
@@ -79,11 +79,42 @@ class Stieger2021(BaseDataset):
     only, up/down (UD) only, and combined 2D movement (2D)30. Each experimental
     block (LR, UD, 2D) consisted of 3 runs, where each run was composed of 25
     trials. After the first three blocks, participants were given a short break
-    (5–10 minutes) that required rating comics by preference. The break task was
+    (5-10 minutes) that required rating comics by preference. The break task was
     chosen to standardize subject experience over the break interval. Following
     the break, participants competed the same 3 blocks as before. In total,
     each session consisted of 2 blocks of each task (6 runs total of LR, UD,
     and 2D control), which culminated in 450 trials performed each day.
+
+    Online BCI control of the cursor proceeded in a series of steps.
+    The first step, feature extraction, consisted of spatial filtering and
+    spectrum estimation. During spatial filtering, the average signal of the 4
+    electrodes surrounding the hand knob of the motor cortex was subtracted from
+    electrodes C3 and C4 to reduce the spatial noise. Following spatial filtering,
+    the power spectrum was estimated by fitting an autoregressive model of order 16
+    to the most recent 160 ms of data using the maximum entropy method. The goal
+    of this method is to find the coefficients of a linear all-pole filter that,
+    when applied to white noise, reproduces the data's spectrum. The main advantage
+    of this method is that it produces high frequency resolution estimates for short
+    segments of data. The parameters are found by minimizing (through least squares)
+    the forward and backward prediction errors on the input data subject to the
+    constraint that the filter used for estimation shares the same autocorrelation
+    sequence as the input data. Thus, the estimated power spectrum directly
+    corresponds to this filter's transfer function divided by the signal's total power.
+    Numerical integration was then used to find the power within a 3 Hz bin
+    centered within the alpha rhythm (12 Hz).
+    The translation algorithm, the next step in the pipeline, then translated the
+    user's alpha power into cursor movement. Horizontal motion was controlled by
+    lateralized alpha power (C4 - C3) and vertical motion was controlled by up
+    and down regulating total alpha power (C4 + C3). These control signals were
+    normalized to zero mean and unit variance across time by subtracting the
+    signals' mean and dividing by its standard deviation. A balanced estimate
+    of the mean and standard deviation of the horizontal and vertical control
+    signals was calcu- lated by estimating these values across time from data
+    derived from 30 s buffers of individual trial type (e.g., the normalized
+    control signal should be positive for right trials and negative for left
+    trials, but the average of left and right trials should be zero). Finally,
+    the normalized control signals were used to update the position of
+    the cursor every 40 ms.
 
     References
     ----------
@@ -108,7 +139,14 @@ class Stieger2021(BaseDataset):
         self.sessions = sessions
         self.figshare_id = 13123148  # id on figshare
 
-        assert interval[0] >= 0.00  # the interval has to start after the cue onset
+        assert interval[0] >= -2.00, (
+            "The epoch interval has to start earlierst at "
+            + f" -2.0 s (specified: {interval[0]:.1f})."
+        )
+        assert interval[1] < 6.00, (
+            "The epoch interval has to end latest at "
+            + f"6.0 s (specified: {interval[1]:.1f})."
+        )
 
     def data_path(
         self, subject, path=None, force_update=False, update_path=None, verbose=None
@@ -133,7 +171,9 @@ class Stieger2021(BaseDataset):
             sub = int(file_name.split("_")[0][1:])
             ses = int(file_name.split("_")[-1].split(".")[0])
 
-            if sub == subject and ses in self.sessions:
+            if sub == subject:
+                if self.sessions is not None and ses not in self.sessions:
+                    continue
                 fpath = os.path.join(basepath, file_name)
                 if not os.path.exists(fpath):
                     pooch.retrieve(
@@ -204,8 +244,11 @@ class Stieger2021(BaseDataset):
             p_keep = np.flatnonzero(stim_flat).shape[0] / container.data.shape[0]
 
             message = (
-                f"Record {subject}/{session} (subject/session): "
-                f"rejecting {(1 - p_keep) * 100:.0f}% of the trials."
+                "The trial length of this dataset is dynamic."
+                f"For the specified interval [{self.interval[0]}, {self.interval[1]}], "
+                f"{(1 - p_keep) * 100:.0f}% of the epochs of record {subject}/"
+                f"{session} (subject/session) were rejected (artifact or"
+                " too short) ."
             )
             if p_keep < 0.5:
                 LOGGER.warning(message)
@@ -231,8 +274,8 @@ class Stieger2021(BaseDataset):
 
             if len(raw.info["bads"]) > 0:
                 LOGGER.info(
-                    "Record {subject}/{session} (subject/session): "
-                    "bad channels that need to be interpolated: {bad_info}",
+                    "Record {subject}/{session} (subject/session) contains "
+                    "bad channels: {bad_info}",
                     subject=subject,
                     session=session,
                     bad_info=raw.info["bads"],
