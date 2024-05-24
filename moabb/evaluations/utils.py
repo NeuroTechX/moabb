0f1d0703b2e2bab42dc3175ebb33b0162407b1b2
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-05-24 11:58:06 +0200

    updating the util

--- moabb/evaluations/utils.py
+++ moabb/evaluations/utils.py
@@ -234,9 +234,16 @@ def create_save_path(
         print("No hdf5_path provided, models will not be saved.")
 
 
-def create_deep_model(clf, learning_rate, weight_decay, drop_rate, epochs):
+def create_deep_model(
+    clf, learning_rate, weight_decay, drop_rate, epochs, manual_validation=False
+):
     keras_clf = clf[-1]
     steps = list(clf.steps)
+
+    if manual_validation:
+        validation_split = (0.0,)
+    else:
+        validation_split = keras_clf.validation_split
 
     Adam = tf.keras.optimizers.Adam(
         learning_rate=learning_rate, weight_decay=weight_decay
@@ -246,15 +253,15 @@ def create_deep_model(clf, learning_rate, weight_decay, drop_rate, epochs):
         optimizer=Adam,
         drop_rate=drop_rate,
         epochs=epochs,
-        verbose=True,
+        verbose=keras_clf.verbose,
         # rest of the parameters are the same as the original model
         callbacks=keras_clf.callbacks,
         random_state=keras_clf.random_state,
         batch_size=keras_clf.batch_size,
-        validation_split=0.0,
+        validation_split=validation_split,
         shuffle=keras_clf.shuffle,
     )
     steps[-1] = ("deep", new_keras_clf)
-    pre_pipe = Pipeline(steps[:-1])
+    pre_process_steps = Pipeline(steps[:-1])
     # pipe = Pipeline(("deep", new_keras_clf))
-    return pre_pipe, new_keras_clf
+    return pre_process_steps, new_keras_clf
