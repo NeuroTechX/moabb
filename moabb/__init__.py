7e10311d8c3c2398b5f3c6450388a9691d5fd854
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-05-30 00:26:46 +0200

    Increase version

--- moabb/__init__.py
+++ moabb/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 
 from .benchmark import benchmark
 from .utils import make_process_pipelines, set_download_dir, set_log_level, setup_seed
