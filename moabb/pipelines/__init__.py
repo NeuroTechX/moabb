0387dc591475bebe84eb417bb204d5051de74d8f
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-06-05 16:40:07 +0200

    fixing the __init__ functions

--- moabb/pipelines/__init__.py
+++ moabb/pipelines/__init__.py
@@ -4,18 +4,16 @@ Pipelines are typically a chain of sklearn compatible transformers and
 end with a sklearn compatible estimator.
 """
 
-# flake8: noqa
-from mne.utils import warn
+from warnings import warn
 
+# flake8: noqa
 from .classification import SSVEP_CCA, SSVEP_TRCA, SSVEP_MsetCCA
 from .features import FM, AugmentedDataset, ExtendedSSVEPSignal, LogVariance
 from .utils import FilterBank, create_pipeline_from_config
 
 
 def __getattr__(name):
-    # ideas from https://stackoverflow.com/a/57110249/1469195
     import importlib
-    from warnings import warn
 
     if name in [
         "KerasDeepConvNet",
@@ -25,14 +23,23 @@ def __getattr__(name):
         "KerasEEGTCNet",
         "KerasShallowConvNet",
     ]:
-        warn(
-            f"{name} is incorrectly import, please use from "
-            f"moabb.pipeline.deep_learning import {name} or"
-            f"instead of moabb.pipeline import {name}",
-            category=DeprecationWarning,
-        )
-        if check_if_tensorflow_install():
+        if _check_if_tensorflow_install():
+            warning_msg = _warning_msg(name, "deep_learning")
+            warn(f"{warning_msg}")
+
             dl_module = importlib.import_module(".deep_learning", __package__)
+            dl_class = dl_module.__dict__[name]
+            return dl_class
+
+    if name in [
+        "EEGNet",
+        "TCN_block",
+    ]:
+        if _check_if_tensorflow_install():
+            warning_msg = _warning_msg(name, "utils_deep_model")
+            warn(f"{warning_msg}")
+
+            dl_module = importlib.import_module(".utils_deep_model", __package__)
             dl_class = dl_module.__dict__[name]
             return dl_class
 
@@ -41,14 +48,8 @@ def __getattr__(name):
         "BraindecodeDatasetLoader",
         "get_shape_from_baseconcat",
     ]:
-        print("AAAAAAA")
-        warn(
-            f"{name} is incorrectly import, please use from "
-            f"moabb.pipeline.utils_pytorch import {name} or"
-            f"instead of moabb.pipeline import {name}",
-            category=DeprecationWarning,
-        )
-        if check_if_braindecode_install():
+        if _check_if_braindecode_install():
+            warn(_warning_msg(name, "utils_pytorch"))
             sub_module = importlib.import_module(".utils_pytorch", __package__)
             class_obj = sub_module.__dict__[name]
             return class_obj
@@ -56,36 +57,48 @@ def __getattr__(name):
     raise AttributeError("No possible import named " + name)
 
 
-def check_if_tensorflow_install():
+def _warning_msg(name, submodule):
+    msg = (
+        f"{name} is incorrectly imported. \nPlease use:\033[1m "
+        f"from moabb.pipeline.{submodule} import {name}\033[0m.\n"
+        f"Instead of: \033[1mfrom moabb.pipeline import {name}\033[0m."
+    )
+    return msg
+
+
+def _check_if_tensorflow_install():
     try:
         import keras
         import scikeras
         import tensorflow
 
         return True
-    except ModuleNotFoundError as err:
+    except ModuleNotFoundError:
         warn(
-            f"{err}\n. You have issues importing tensorflow or keras.\n"
-            "You won't be able to use these deep learning pipelines if you "
-            "attempt to do so. Please run `pip install moabb[deeplearning]`, if you"
-            "want to use these pipelines.",
-            category=ModuleNotFoundError,
-            module="moabb.pipelines",
+            "\nThere was a problem importing tensorflow or keras, "
+            "which are required for the deep learning pipelines. \n"
+            "The Keras MOABB deep learning pipelines cannot be used.\n "
+            "To resolve this issue, please install the necessary dependencies "
+            "by running the following command in your terminal: \n"
+            "\033[94m"  # This is the ANSI escape code for green
+            "pip install moabb[deeplearning]"
+            "\033[0m",  # This resets the color back to normal
         )
         return False
 
 
-def check_if_braindecode_install():
+def _check_if_braindecode_install():
     try:
         import braindecode
 
         return True
-    except ModuleNotFoundError as err:
+    except ModuleNotFoundError:
         warn(
             "Braindecode is not installed. "
             "You won't be able to use these braindecode function if you "
-            "attempt to do. Please install `braindecode`",
-            category=ModuleNotFoundError,
-            module="moabb.pipelines",
+            "attempt to do. \n"
+            "\033[94m"  # This is the ANSI escape code for green
+            "pip install braindecode"
+            "\033[0m",  # This resets the color back to normal
         )
         return False
