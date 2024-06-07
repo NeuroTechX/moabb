052ce750d16edb72dad6cbd2dd4d384be094b5e2
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-06-07 22:51:44 +0200

    restaring test

--- moabb/tests/util_tests.py
+++ moabb/tests/util_tests.py
@@ -2,8 +2,10 @@ import os.path as osp
 import unittest
 from unittest.mock import MagicMock, patch
 
+import pytest
 from mne import get_config
 
+from moabb.datasets import utils
 from moabb.utils import aliases_list, depreciated_alias, set_download_dir, setup_seed
 
 
@@ -23,49 +25,53 @@ class TestDownload(unittest.TestCase):
         set_download_dir(original_path)
 
 
-# class Test_Utils(unittest.TestCase):
-#     def test_channel_intersection_fun(self):
-#         print(utils.find_intersecting_channels([d() for d in utils.dataset_list])[0])
-#
-#     def test_dataset_search_fun(self):
-#         found = utils.dataset_search("imagery", multi_session=True)
-#         print([type(dataset).__name__ for dataset in found])
-#         found = utils.dataset_search("imagery", multi_session=False)
-#         print([type(dataset).__name__ for dataset in found])
-#         res = utils.dataset_search(
-#             "imagery", events=["right_hand", "left_hand", "feet", "tongue", "rest"]
-#         )
-#         for out in res:
-#             print("multiclass: {}".format(out.event_id.keys()))
-#
-#         res = utils.dataset_search(
-#             "imagery", events=["right_hand", "feet"], has_all_events=True
-#         )
-#         for out in res:
-#             self.assertTrue(set(["right_hand", "feet"]) <= set(out.event_id.keys()))
-#
-#     def test_dataset_channel_search(self):
-#         chans = ["C3", "Cz"]
-#         All = utils.dataset_search(
-#             "imagery", events=["right_hand", "left_hand", "feet", "tongue", "rest"]
-#         )
-#         has_chans = utils.dataset_search(
-#             "imagery",
-#             events=["right_hand", "left_hand", "feet", "tongue", "rest"],
-#             channels=chans,
-#         )
-#         has_types = set([type(x) for x in has_chans])
-#         for d in has_chans:
-#             s1 = d.get_data([1])[1]
-#             sess1 = s1[list(s1.keys())[0]]
-#             raw = sess1[list(sess1.keys())[0]]
-#             self.assertTrue(set(chans) <= set(raw.info["ch_names"]))
-#         for d in All:
-#             if type(d) not in has_types:
-#                 s1 = d.get_data([1])[1]
-#                 sess1 = s1[list(s1.keys())[0]]
-#                 raw = sess1[list(sess1.keys())[0]]
-#                 self.assertFalse(set(chans) <= set(raw.info["ch_names"]))
+@pytest.mark.skip(
+    reason="This test is only when you have already " "download the datasets."
+)
+class Test_Utils(unittest.TestCase):
+
+    def test_channel_intersection_fun(self):
+        print(utils.find_intersecting_channels([d() for d in utils.dataset_list])[0])
+
+    def test_dataset_search_fun(self):
+        found = utils.dataset_search("imagery", multi_session=True)
+        print([type(dataset).__name__ for dataset in found])
+        found = utils.dataset_search("imagery", multi_session=False)
+        print([type(dataset).__name__ for dataset in found])
+        res = utils.dataset_search(
+            "imagery", events=["right_hand", "left_hand", "feet", "tongue", "rest"]
+        )
+        for out in res:
+            print("multiclass: {}".format(out.event_id.keys()))
+
+        res = utils.dataset_search(
+            "imagery", events=["right_hand", "feet"], has_all_events=True
+        )
+        for out in res:
+            self.assertTrue(set(["right_hand", "feet"]) <= set(out.event_id.keys()))
+
+    def test_dataset_channel_search(self):
+        chans = ["C3", "Cz"]
+        All = utils.dataset_search(
+            "imagery", events=["right_hand", "left_hand", "feet", "tongue", "rest"]
+        )
+        has_chans = utils.dataset_search(
+            "imagery",
+            events=["right_hand", "left_hand", "feet", "tongue", "rest"],
+            channels=chans,
+        )
+        has_types = set([type(x) for x in has_chans])
+        for d in has_chans:
+            s1 = d.get_data([1])[1]
+            sess1 = s1[list(s1.keys())[0]]
+            raw = sess1[list(sess1.keys())[0]]
+            self.assertTrue(set(chans) <= set(raw.info["ch_names"]))
+        for d in All:
+            if type(d) not in has_types:
+                s1 = d.get_data([1])[1]
+                sess1 = s1[list(s1.keys())[0]]
+                raw = sess1[list(sess1.keys())[0]]
+                self.assertFalse(set(chans) <= set(raw.info["ch_names"]))
 
 
 class TestSetupSeed(unittest.TestCase):
@@ -127,6 +133,18 @@ class TestDepreciatedAlias(unittest.TestCase):
         self.assertIsInstance(b, DummyB)  # noqa: F821
         self.assertIsInstance(b, DummyA)
 
+        # assertNoLogs was added in Python 3.10
+        # https://bugs.python.org/issue39385
+        if hasattr(self, "assertNoLogs"):
+            with self.assertNoLogs(logger="moabb.utils", level="WARN") as cm:
+                a = DummyA(2, b=2)
+            self.assertEqual(
+                a.__doc__,
+                "DummyA class\n\n    Notes\n    -----\n\n"
+                "    .. note:: ``DummyA`` was previously named ``DummyB``. "
+                "``DummyB`` will be removed in  version 0.1.\n",
+            )
+
     def test_class_alias_notes(self):
         @depreciated_alias("DummyB", expire_version="0.1")
         class DummyA:
@@ -146,6 +164,17 @@ class TestDepreciatedAlias(unittest.TestCase):
 
         self.assertIn(("DummyB", "DummyA", "0.1"), aliases_list)
 
+        if hasattr(self, "assertNoLogs"):
+            with self.assertNoLogs(logger="moabb.utils", level="WARN"):
+                a = DummyA(2, b=2)
+            self.assertEqual(
+                a.__doc__,
+                "DummyA class\n\n            Notes\n            -----\n\n"
+                "            .. note:: ``DummyA`` was previously named ``DummyB``. "
+                "``DummyB`` will be removed in  version 0.1.\n\n"
+                "            a note",
+            )
+
     def test_function_alias(self):
         @depreciated_alias("dummy_b", expire_version="0.1")
         def dummy_a(a, b=1):
@@ -163,6 +192,9 @@ class TestDepreciatedAlias(unittest.TestCase):
             "    .. note:: ``dummy_a`` was previously named ``dummy_b``. "
             "``dummy_b`` will be removed in  version 0.1.\n",
         )
+        if hasattr(self, "assertNoLogs"):
+            with self.assertNoLogs(logger="moabb.utils", level="WARN") as cm:
+                self.assertEqual(dummy_a(2, b=2), 4)
 
         with self.assertLogs(logger="moabb.utils", level="WARN") as cm:
             self.assertEqual(dummy_b(2, b=2), 4)  # noqa: F821
