51bf54941f431ca0f92fe68086892424890e79b2
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2024-06-08 00:20:37 +0200

    trying another approach

--- moabb/pipelines/__init__.py
+++ moabb/pipelines/__init__.py
@@ -13,60 +13,54 @@ from .utils import FilterBank, create_pipeline_from_config
 
 
 def __getattr__(name):
-    import importlib
 
-    if name in [
+    deep_learning_classes = {
         "KerasDeepConvNet",
         "KerasEEGITNet",
         "KerasEEGNet_8_2",
         "KerasEEGNeX",
         "KerasEEGTCNet",
         "KerasShallowConvNet",
-    ]:
-        if _check_if_tensorflow_install():
-            warning_msg = _warning_msg(name, "deep_learning")
-            warn(f"{warning_msg}")
-
-            dl_module = importlib.import_module(".deep_learning", __package__)
-            dl_class = dl_module.__dict__[name]
-            return dl_class
-
-    if name in [
+    }
+    utils_deep_model_classes = {
         "EEGNet",
         "TCN_block",
-    ]:
-        if _check_if_tensorflow_install():
-            warning_msg = _warning_msg(name, "utils_deep_model")
-            warn(f"{warning_msg}")
-
-            dl_module = importlib.import_module(".utils_deep_model", __package__)
-            dl_class = dl_module.__dict__[name]
-            return dl_class
-
-    if name in [
+    }
+    utils_pytorch_classes = {
         "InputShapeSetterEEG",
         "BraindecodeDatasetLoader",
         "get_shape_from_baseconcat",
-    ]:
-        if _check_if_braindecode_install():
-            warn(_warning_msg(name, "utils_pytorch"))
-            sub_module = importlib.import_module(".utils_pytorch", __package__)
-            class_obj = sub_module.__dict__[name]
-            return class_obj
+    }
 
-    raise AttributeError("No possible import named " + name)
+    if name in deep_learning_classes and _check_if_tensorflow_installed():
+        return _import_class(name, ".deep_learning")
+    elif name in utils_deep_model_classes and _check_if_tensorflow_installed():
+        return _import_class(name, ".utils_deep_model")
+    elif name in utils_pytorch_classes and _check_if_braindecode_installed():
+        return _import_class(name, ".utils_pytorch")
+
+    raise AttributeError(f"Module '{__name__}' has no attribute '{name}'")
+
+
+def _import_class(name, module_name):
+    import importlib
+
+    warning_msg = _warning_msg(name, module_name)
+    warn(warning_msg)
+
+    module = importlib.import_module(module_name, __package__)
+    return getattr(module, name)
 
 
 def _warning_msg(name, submodule):
-    msg = (
+    return (
         f"{name} is incorrectly imported. \nPlease use:\033[1m "
-        f"from moabb.pipeline.{submodule} import {name}\033[0m.\n"
+        f"from moabb.pipeline{submodule} import {name}\033[0m.\n"
         f"Instead of: \033[1mfrom moabb.pipeline import {name}\033[0m."
     )
-    return msg
 
 
-def _check_if_tensorflow_install():
+def _check_if_tensorflow_installed():
     try:
         import keras
         import scikeras
@@ -80,14 +74,14 @@ def _check_if_tensorflow_install():
             "The Keras MOABB deep learning pipelines cannot be used.\n "
             "To resolve this issue, please install the necessary dependencies "
             "by running the following command in your terminal: \n"
-            "\033[94m"  # This is the ANSI escape code for green
+            "\033[94m"  # This is the ANSI escape code for blue
             "pip install moabb[deeplearning]"
             "\033[0m",  # This resets the color back to normal
         )
         return False
 
 
-def _check_if_braindecode_install():
+def _check_if_braindecode_installed():
     try:
         import braindecode
 
@@ -95,9 +89,9 @@ def _check_if_braindecode_install():
     except ModuleNotFoundError:
         warn(
             "Braindecode is not installed. "
-            "You won't be able to use these braindecode function if you "
-            "attempt to do. \n"
-            "\033[94m"  # This is the ANSI escape code for green
+            "You won't be able to use these braindecode functions if you "
+            "attempt to do so. \n"
+            "\033[94m"  # This is the ANSI escape code for blue
             "pip install braindecode"
             "\033[0m",  # This resets the color back to normal
         )
