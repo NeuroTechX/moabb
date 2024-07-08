3bdf2b1b491c466772218c855ac10b210854ef9e
Pierre Guetschel <pierre.guetschel@donders.ru.nl>
2024-07-08 23:33:46 +0200

    Add description to all docstrings

--- moabb/datasets/erpcore2021.py
+++ moabb/datasets/erpcore2021.py
@@ -59,11 +59,9 @@ DATASET_PARAMS = {
     for task, osf in OSF_IDS.items()
 }
 
+_docstring_head = """
 
-class Erpcore2021(BaseDataset):
-    """Abstract base dataset class for Erpcore2021.
-
-    Datasets [1]_ from the article [2]_.
+    Datasets [1]_ from the study [2]_.
 
     **Dataset Description**
 
@@ -76,7 +74,38 @@ class Erpcore2021(BaseDataset):
     (MMN), N2pc, N400, P3, Lateralized Readiness Potential (LRP), and Error-Related Negativity
     (ERN). These experiments were conducted to standardize ERP paradigms and protocols across
     studies.
+"""
+_docstring_tail = """
+    The continuous EEG was recorded using a Biosemi ActiveTwo recording system with active
+    electrodes (Biosemi B.V., Amsterdam, the Netherlands). Recording from 30 scalp electrodes,
+    mounted in an elastic cap and placed according to the International 10/20 System (FP1, F3,
+    F7, FC3, C3, C5, P3, P7, P9, PO7, PO3, O1, Oz, Pz, CPz, FP2, Fz, F4, F8, FC4, FCz, Cz, C4,
+    C6, P4, P8, P10, PO8, PO4, O2; see Supplementary Fig. S1). The common mode sense (CMS)
+    electrode was located at PO1, and the driven right leg (DRL) electrode was located at PO2.
+    The horizontal electrooculogram (HEOG) was recorded from electrodes placed lateral to the
+    external canthus of each eye. The vertical electrooculogram (VEOG) was recorded from an
+    electrode placed below the right eye. Signals were incidentally also recorded from 37 other
+    sites, but these sites were not monitored during the recording and are not included in
+    the ERP CORE data set. All signals were low-pass filtered using a fifth order sinc filter
+    with a half-power cutoff at 204.8 Hz and then digitized at 1024 Hz with 24 bits of resolution.
+    The signals were recorded in single-ended mode (i.e., measuring the voltage between the active
+    and ground electrodes without the use of a reference), and referencing was performed offline.
 
+    References
+    ----------
+    .. [1] Emily S. Kappenman, Jaclyn L. Farrens, Wendy Zhang, Andrew X. Stewart, Steven J. Luck.
+        (2020). ERP CORE: An open resource for human event-related potential research. NeuroImage.
+        DOI: https://doi.org/10.1016/j.neuroimage.2020.117465
+
+    .. [2] Emily S. Kappenman, Jaclyn L. Farrens, Wendy Zhang, Andrew X. Stewart, Steven J. Luck.
+        ERP CORE: An open resource for human event-related potential research.
+        DOI: https://doi.org/10.1016/j.neuroimage.2020.117465
+"""
+
+
+class Erpcore2021(BaseDataset):
+    __doc__ = f"""Abstract base dataset class the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
     **Experimental procedures**:
     - **N170**: Subjects viewed faces and objects to elicit the N170 component. In this task,
     an image of a face, car, scrambled face, or scrambled car was presented on each trial in
@@ -102,33 +131,7 @@ class Erpcore2021(BaseDataset):
     sides by arrowheads that pointed in the same direction (congruent trials) or the opposite
     direction (incongruent trials). Participants indicated the direction of the central
     arrowhead on each trial with a left- or right-hand buttonpress.
-
-
-    The continuous EEG was recorded using a Biosemi ActiveTwo recording system with active
-    electrodes (Biosemi B.V., Amsterdam, the Netherlands). Recording from 30 scalp electrodes,
-    mounted in an elastic cap and placed according to the International 10/20 System (FP1, F3,
-    F7, FC3, C3, C5, P3, P7, P9, PO7, PO3, O1, Oz, Pz, CPz, FP2, Fz, F4, F8, FC4, FCz, Cz, C4,
-    C6, P4, P8, P10, PO8, PO4, O2; see Supplementary Fig. S1). The common mode sense (CMS)
-    electrode was located at PO1, and the driven right leg (DRL) electrode was located at PO2.
-    The horizontal electrooculogram (HEOG) was recorded from electrodes placed lateral to the
-    external canthus of each eye. The vertical electrooculogram (VEOG) was recorded from an
-    electrode placed below the right eye. Signals were incidentally also recorded from 37 other
-    sites, but these sites were not monitored during the recording and are not included in
-    the ERP CORE data set. All signals were low-pass filtered using a fifth order sinc filter
-    with a half-power cutoff at 204.8 Hz and then digitized at 1024 Hz with 24 bits of resolution.
-    The signals were recorded in single-ended mode (i.e., measuring the voltage between the active
-    and ground electrodes without the use of a reference), and referencing was performed offline.
-
-    References
-    ----------
-    .. [1] Emily S. Kappenman, Jaclyn L. Farrens, Wendy Zhang, Andrew X. Stewart, Steven J. Luck.
-        (2020). ERP CORE: An open resource for human event-related potential research. NeuroImage.
-        DOI: https://doi.org/10.18115/D5JW4R
-
-    .. [2] Emily S. Kappenman, Jaclyn L. Farrens, Wendy Zhang, Andrew X. Stewart, Steven J. Luck.
-        ERP CORE: An open resource for human event-related potential research.
-        DOI: https://doi.org/10.1016/j.neuroimage.2020.117465
-
+    {_docstring_tail}
     """
 
     def __init__(self, task):
@@ -417,15 +420,14 @@ class Erpcore2021(BaseDataset):
 
 
 class Erpcore2021_N170(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""N170 events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     Subjects viewed faces and objects to elicit the N170 component. In this task,
     an image of a face, car, scrambled face, or scrambled car was presented on each trial in
     the center of the screen, and participants responded whether the stimulus was an “object”
     (face or car) or a “texture” (scrambled face or scrambled car).
-
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "N170")
@@ -461,14 +463,14 @@ class Erpcore2021_N170(Erpcore2021):
 
 
 class Erpcore2021_MMN(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""MMN events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     Subjects were exposed to a sequence of auditory stimuli to evoke the mismatch
     negativity response, indicating automatic detection of deviant sounds.  Standard tones
     (presented at 80 dB, with p = .8) and deviant tones (presented at 70 dB, with p = .2)
     were presented over speakers while participants watched a silent video and ignored the tones.
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "MMN")
@@ -500,13 +502,13 @@ class Erpcore2021_MMN(Erpcore2021):
 
 
 class Erpcore2021_N2pc(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""N2pc events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     Participants were given a target color of pink or blue at the beginning of a
     trial block, and responded on each trial whether the gap in the target color square was
     on the top or bottom.
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "N2pc")
@@ -559,16 +561,16 @@ class Erpcore2021_N2pc(Erpcore2021):
 
 
 class Erpcore2021_P3(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""P3 events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     The letters A, B, C, D, and E were presented in random order (p = .2 for each
     letter). One letter was designated the target for a given block of trials, and the other
     4 letters were non-targets. Thus, the probability of the target category was .2, but the
     same physical stimulus served as a target in some blocks and a nontarget in others.
     Participants responded whether the letter presented on each trial was the target or a
     non-target for that block.
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "P3")
@@ -671,13 +673,13 @@ class Erpcore2021_P3(Erpcore2021):
 
 
 class Erpcore2021_N400(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""N400 events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     On each trial, a red prime word was followed by a green target word.
     Participants responded whether the target word was semantically related or unrelated
     to the prime word.
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "N400")
@@ -721,14 +723,14 @@ class Erpcore2021_N400(Erpcore2021):
 
 
 class Erpcore2021_ERN(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""ERN events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     A central arrowhead pointing to the left or right was flanked on both
     sides by arrowheads that pointed in the same direction (congruent trials) or the opposite
     direction (incongruent trials). Participants indicated the direction of the central
     arrowhead on each trial with a left- or right-hand buttonpress.
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "ERN")
@@ -803,14 +805,14 @@ class Erpcore2021_ERN(Erpcore2021):
 
 
 class Erpcore2021_LRP(Erpcore2021):
-    """
-
-    Description of the task:
-
+    __doc__ = f"""LRP events of the ERP CORE dataset by Kappenman et al. 2020.
+    {_docstring_head}
+    **Experimental procedure**:
     A central arrowhead pointing to the left or right was flanked on both
     sides by arrowheads that pointed in the same direction (congruent trials) or the opposite
     direction (incongruent trials). Participants indicated the direction of the central
     arrowhead on each trial with a left- or right-hand buttonpress.
+    {_docstring_tail}
     """
 
     __init__ = partialmethod(Erpcore2021.__init__, "LRP")
