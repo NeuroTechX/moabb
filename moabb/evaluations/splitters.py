88ee9106654dd59d334ea34d2bb5bf3051194d4b
brunalopes <brunaafl@usp.br>
2024-10-18 12:26:44 +0200

    Add shuffle and random_state parameters to WithinSession

--- moabb/evaluations/splitters.py
+++ moabb/evaluations/splitters.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import (
     StratifiedKFold,
 )
 
+from sklearn.utils import check_random_state
+
 
 class WithinSessionSplitter(BaseCrossValidator):
     """Data splitter for within session evaluation.
@@ -56,109 +58,47 @@ class WithinSessionSplitter(BaseCrossValidator):
     Fold 3:
       Train: index=[0 1], group=[1 1], session=['T' 'T']
       Test:  index=[4 5], group=[1 1], sessions=['T' 'T']
-
-
     """
 
-    def __init__(self, n_folds=5, random_state=42, shuffle=True):
-
+    def __init__(self, n_folds:int=5, random_state:int=42, shuffle:bool=True):
         # Check type
         assert isinstance(n_folds, int)
 
         self.n_folds = n_folds
         # Setting random state
-        self.random_state = random_state
+        self.random_state = check_random_state(random_state)
+
         self.shuffle = shuffle
 
     def get_n_splits(self, metadata):
         sessions_subjects = metadata.groupby(["subject", "session"]).ngroups
         return self.n_folds * sessions_subjects
 
-    def split(self, X, y, metadata, **kwargs):
+    def split(self, y, metadata, **kwargs):
 
         assert isinstance(self.n_folds, int)
 
+        all_index = metadata.index.values
         subjects = metadata.subject.values
         cv = StratifiedKFold(n_splits=self.n_folds, shuffle=self.shuffle,
                              random_state=self.random_state)
 
         for subject in np.unique(subjects):
             mask = subjects == subject
-            X_, y_, meta_ = (
-                X[mask],
+            index_subject, y_subject, meta_subject = (
+                all_index[mask],
                 y[mask],
                 metadata[mask],
             )
 
-            sessions = meta_.session.values
+            sessions = meta_subject.session.values
 
             for session in np.unique(sessions):
-                mask_s = sessions == session
-                X_s, y_s, _ = (
-                    X_[mask_s],
-                    y_[mask_s],
-                    meta_[mask_s],
+                mask_session = sessions == session
+                index_session, y_session = (
+                    index_subject[mask_session],
+                    y_subject[mask_session]
                 )
 
-                for ix_train, ix_test in cv.split(X_s, y_s):
-
-                    ix_train_global = np.where(mask)[0][np.where(mask_s)[0][ix_train]]
-                    ix_test_global = np.where(mask)[0][np.where(mask_s)[0][ix_test]]
-                    yield ix_train_global, ix_test_global
-
-
-class IndividualWithinSessionSplitter(BaseCrossValidator):
-    """Data splitter for within session evaluation.
-
-    Within-session evaluation uses k-fold cross_validation to determine train
-    and test sets on separate session for each subject. This splitter does not assume
-    that all data and metadata from all subjects is already loaded. If X, y and metadata
-    are from a single subject, it returns data split for this subject only.
-
-    It can be used as basis for WithinSessionSplitter or to avoid downloading all data at
-    once when it is not needed,
-
-    Parameters
-    ----------
-    n_folds : int
-        Number of folds. Must be at least 2.
-    random_state: int, RandomState instance or None, default=None
-        Important when `shuffle` is True. Controls the randomness of splits.
-        Pass an int for reproducible output across multiple function calls.
-    shuffle : bool, default=True
-        Whether to shuffle each class's samples before splitting into batches.
-        Note that the samples within each split will not be shuffled.
-
-    """
-
-    def __init__(self, n_folds=5, random_state=42, shuffle=True):
-        # Check type
-        assert isinstance(n_folds, int)
-
-        self.n_folds = n_folds
-        # Setting random state
-        self.random_state = random_state
-        self.shuffle = shuffle
-
-    def get_n_splits(self, metadata):
-        return self.n_folds
-
-    def split(self, X, y, metadata, **kwargs):
-
-        assert len(np.unique(metadata.subject)) == 1
-        assert isinstance(self.n_folds, int)
-
-        sessions = metadata.subject.values
-        cv = StratifiedKFold(n_splits=self.n_folds, shuffle=self.shuffle,
-                             random_state=self.random_state)
-
-        for session in np.unique(sessions):
-            mask = sessions == session
-            X_, y_, _ = (
-                X[mask],
-                y[mask],
-                metadata[mask],
-            )
-
-            for ix_train, ix_test in cv.split(X_, y_):
-                yield ix_train, ix_test
+                for ix_train, ix_test in cv.split(index_session, y_session):
+                    yield index_session[ix_train], index_session[ix_test]
