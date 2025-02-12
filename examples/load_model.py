649a99e6cadc3b50481f9e7ef154c01bc766cf3d
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2025-02-12 11:30:18 +0100

    removing scikeras

--- examples/load_model.py
+++ examples/load_model.py
@@ -1,6 +1,6 @@
 """
 ==============================================
-Load Model (Scikit, Keras) with MOABB
+Load Model with MOABB
 ==============================================
 
 This example shows how to use load the pretrained pipeline in MOABB.
@@ -12,13 +12,7 @@ This example shows how to use load the pretrained pipeline in MOABB.
 
 from pickle import load
 
-import keras
-from scikeras.wrappers import KerasClassifier
-from sklearn.pipeline import Pipeline
-
 from moabb import set_log_level
-from moabb.pipelines.features import StandardScaler_Epoch
-from moabb.utils import setup_seed
 
 
 set_log_level("info")
@@ -27,12 +21,8 @@ set_log_level("info")
 # In this example, we will use the results computed by the following examples
 #
 # - plot_benchmark_
-# - plot_benchmark_braindecode_
-# - plot_benchmark_DL_
 # ---------------------
 
-# Set up reproducibility of Tensorflow and PyTorch
-setup_seed(42)
 
 ###############################################################################
 # Loading the Scikit-learn pipelines
@@ -42,22 +32,3 @@ with open(
     "rb",
 ) as pickle_file:
     CSP_SVM_Trained = load(pickle_file)
-
-###############################################################################
-# Loading the Keras model
-# We load the single Keras model, if we want we can set in the exact same pipeline.
-
-model_Keras = keras.models.load_model(
-    "./results/Models_WithinSession/BNCI2014-001/1/1E/Keras_DeepConvNet/kerasdeepconvnet_fitted_model_best.h5"
-)
-# Now we need to instantiate a new SciKeras object since we only saved the Keras model
-Keras_DeepConvNet_Trained = KerasClassifier(model_Keras)
-# Create the pipelines
-
-
-pipes_keras = Pipeline(
-    [
-        ("StandardScaler_Epoch", StandardScaler_Epoch),
-        ("Keras_DeepConvNet_Trained", Keras_DeepConvNet_Trained),
-    ]
-)
