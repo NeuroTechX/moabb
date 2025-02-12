0c957130c6c79b86451c93af3de47b914a7c8d1e
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2025-02-12 11:27:08 +0100

    removing util deep learning from moabb

--- moabb/utils.py
+++ moabb/utils.py
@@ -37,64 +37,8 @@ def _set_random_seed(seed: int) -> None:
     np.random.seed(seed)
 
 
-def _set_tensorflow_seed(seed: int) -> None:
-    """Set the seed for TensorFlow.
-
-    Parameters
-    ----------
-    seed: int
-        The random seed to use.
-    Returns
-    -------
-    None
-    """
-    try:
-        import tensorflow as tf
-
-        tf.random.set_seed(seed)  # tf cpu fix seed
-        os.environ["TF_DETERMINISTIC_OPS"] = (
-            "1"  # tf gpu fix seed, please `pip install tensorflow-determinism` first
-        )
-        tf.keras.utils.set_random_seed(seed)
-
-    except ImportError:
-        print(
-            "We try to set the tensorflow seeds, but it seems that tensorflow is not installed. "
-            "Please refer to `https://www.tensorflow.org/` to install if you need to use "
-            "this deep learning module."
-        )
-        return False
-
-
-def _set_torch_seed(seed: int) -> None:
-    """Set the seed for PyTorch.
-
-    Parameters
-    ----------
-    seed: int
-        The random seed to use.
-    Returns
-    -------
-    None
-    """
-    try:
-        import torch
-
-        torch.manual_seed(seed)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed(seed)
-            torch.cuda.manual_seed_all(seed)
-    except ImportError:
-        print(
-            "We try to set the torch seeds, but it seems that torch is not installed. "
-            "Please refer to `https://pytorch.org/` to install if you need to use "
-            "this deep learning module."
-        )
-        return False
-
-
 def setup_seed(seed: int) -> None:
-    """Set the seed for random, numpy, TensorFlow and PyTorch.
+    """Set the seed for random, numpy.
 
     Parameters
     ----------
@@ -105,14 +49,8 @@ def setup_seed(seed: int) -> None:
     None
     """
     _set_random_seed(seed)
-    # check if the return is bool
-    tensorflow_return = _set_tensorflow_seed(seed)
-    torch_return = _set_torch_seed(seed)
 
-    if tensorflow_return is False or torch_return is False:
-        return False
-    else:
-        return None
+    return None
 
 
 def set_log_level(level="INFO"):
