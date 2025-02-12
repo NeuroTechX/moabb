3045f25e690b06e244f28cd932175d82e64acbac
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2025-02-12 12:58:44 +0100

    - to _

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,7 +67,7 @@ extensions = [
     "sphinx_design",
     "sphinx_gallery.gen_gallery",
     "gh_substitutions",
-    "myst-parser",
+    "myst_parser",
     "numpydoc",
     "sphinx_favicon",
     "sphinxcontrib.jquery",
