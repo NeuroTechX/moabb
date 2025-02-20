15c297670c23bc99227d2f4dd8f9a90819a7db4a
tommoral <thomas.moreau.2010@gmail.com>
2025-02-20 15:40:52 +0100

    CLN clearer specification for whithin session inner cv_class

--- moabb/evaluations/splitters.py
+++ moabb/evaluations/splitters.py
@@ -13,32 +13,41 @@ class WithinSessionSplitter(BaseCrossValidator):
         :alt: The schematic diagram of the WithinSession split
         :align: center
 
+    The inner cross-validation strategy can be changed by passing the
+    `cv_class` and `cv_kwargs` arguments. By default, it uses StratifiedKFold.
+
     Parameters
     ----------
-    cross_val: cros-validation class, default=StratifiedKFold
-        Inner cross-validation strategy for splitting the sessions.
     n_folds : int, default=5
         Number of folds. Must be at least 2. If
-    random_state: int, RandomState instance or None, default=None
-        Controls the randomness of splits. Only used when `shuffle` is True.
-        Pass an int for reproducible output across multiple function calls.
     shuffle : bool, default=True
         Whether to shuffle each class's samples before splitting into batches.
         Note that the samples within each split will not be shuffled.
+    random_state: int, RandomState instance or None, default=None
+        Controls the randomness of splits. Only used when `shuffle` is True.
+        Pass an int for reproducible output across multiple function calls.
+    cv_class: cros-validation class, default=StratifiedKFold
+        Inner cross-validation strategy for splitting the sessions.
+    cv_kwargs: dict
+        Additional arguments to pass to the inner cross-validation strategy.
 
     """
 
     def __init__(
         self,
-        cross_val: type[BaseCrossValidator] = StratifiedKFold,
         n_folds: int = 5,
-        random_state: int = 42,
         shuffle: bool = True,
+        random_state: int = 42,
+        cv_class: type[BaseCrossValidator] = StratifiedKFold,
+        **cv_kwargs: dict,
     ):
-        self.cross_val = cross_val
+        self.cv_class = cv_class
         self.n_folds = n_folds
         self.shuffle = shuffle
-        self.rng = check_random_state(random_state) if shuffle else None
+        self.cv_kwargs = cv_kwargs
+
+        self.random_state = random_state
+        self._rng = check_random_state(random_state)
 
     def get_n_splits(self, metadata):
         num_sessions_subjects = metadata.groupby(["subject", "session"]).ngroups
@@ -46,28 +55,34 @@ class WithinSessionSplitter(BaseCrossValidator):
 
     def split(self, y, metadata):
         all_index = metadata.index.values
+
+        # Shuffle subjects if required
         subjects = metadata["subject"].unique()
+        if self.shuffle:
+            self._rng.shuffle(subjects)
 
         for subject in subjects:
             subject_mask = metadata["subject"] == subject
             subject_indices = all_index[subject_mask]
             subject_metadata = metadata[subject_mask]
-            sessions = subject_metadata["session"].unique()
             y_subject = y[subject_mask]
 
             # Shuffle sessions if required
+            sessions = subject_metadata["session"].unique()
             if self.shuffle:
-                self.rng.shuffle(sessions)
+                self._rng.shuffle(sessions)
 
             for session in sessions:
                 session_mask = subject_metadata["session"] == session
                 indices = subject_indices[session_mask]
                 y_session = y_subject[session_mask]
 
-                splitter = self.cross_val(
+                # Instanciate a new internal splitter for each session
+                splitter = self.cv_class(
                     n_splits=self.n_folds,
                     shuffle=self.shuffle,
-                    random_state=self.rng,
+                    **self.cv_kwargs,
+                    random_state=self._rng,
                 )
 
                 # Split using the current instance of StratifiedKFold by default
