784313083782455cdd69ccd1c42b4181c09ebbf7
tommoral <thomas.moreau.2010@gmail.com>
2025-02-21 14:48:33 +0100

    FIX properly setup pytest

--- moabb/tests/test_classification.py
+++ moabb/tests/test_classification.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from sklearn.utils.validation import NotFittedError, check_is_fitted
 
@@ -7,8 +7,8 @@ from moabb.paradigms import SSVEP
 from moabb.pipelines import SSVEP_MsetCCA
 
 
-class TestSSVEP_MsetCCA(unittest.TestCase):
-    def setUp(self):
+class TestSSVEP_MsetCCA:
+    def setup(self):
         # Use moabb generated dataset for test
         dataset = FakeDataset(n_sessions=1, n_runs=1, n_subjects=1, paradigm="ssvep")
         paradigm = SSVEP(n_classes=3)
@@ -21,27 +21,26 @@ class TestSSVEP_MsetCCA(unittest.TestCase):
 
     def test_fit(self):
         self.clf.fit(self.X, self.y)
-        self.assertTrue(hasattr(self.clf, "classes_"))
-        self.assertTrue(hasattr(self.clf, "one_hot"))
-        self.assertTrue(hasattr(self.clf, "Ym"))
+        assert hasattr(self.clf, "classes_")
+        assert hasattr(self.clf, "one_hot")
+        assert hasattr(self.clf, "Ym")
 
     def test_predict(self):
         self.clf.fit(self.X, self.y)
         y_pred = self.clf.predict(self.X)
-        self.assertEqual(len(y_pred), len(self.X))
+        assert len(y_pred) == len(self.X)
 
     def test_predict_proba(self):
         self.clf.fit(self.X, self.y)
         P = self.clf.predict_proba(self.X)
-        self.assertEqual(P.shape[0], len(self.X))
-        self.assertEqual(P.shape[1], len(self.freqs))
+        assert P.shape[0] == len(self.X)
+        assert P.shape[1] == len(self.freqs)
 
     def test_fit_predict_is_fitted(self):
-        self.assertRaises(NotFittedError, self.clf.predict, self.X)
-        self.assertRaises(NotFittedError, self.clf.predict_proba, self.X)
+        with pytest.raises(NotFittedError):
+            self.clf.predict(self.X)
+        with pytest.raises(NotFittedError):
+            self.clf.predict_proba(self.X)
+
         self.clf.fit(self.X, self.y)
         check_is_fitted(self.clf, attributes=["classes_", "one_hot", "Ym"])
-
-
-if __name__ == "__main__":
-    unittest.main()
