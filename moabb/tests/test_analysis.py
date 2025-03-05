0774b82e9e8e0502617e12b9a85f7efcb17fcc79
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2025-03-05 13:41:06 +0100

    skipped test to release

--- moabb/tests/test_analysis.py
+++ moabb/tests/test_analysis.py
@@ -2,9 +2,9 @@ import os
 import shutil
 from pathlib import Path
 
-import pytest
 import numpy as np
 import pandas as pd
+import pytest
 from matplotlib.pyplot import Figure
 
 import moabb.analysis.meta_analysis as ma
