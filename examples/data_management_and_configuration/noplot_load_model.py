a69f869b47c4113c2e30de7b72175b12712f2ca0
bruAristimunha <a.bruno@aluno.ufabc.edu.br>
2025-03-07 11:07:52 +0100

    renaming

--- examples/data_management_and_configuration/noplot_load_model.py
+++ examples/data_management_and_configuration/noplot_load_model.py
@@ -21,7 +21,7 @@ set_log_level("info")
 ###############################################################################
 # In this example, we will use the results computed by the following examples
 #
-# - plot_benchmark_
+# - plot_benchmark
 # ---------------------
 
 # Set up reproducibility of Tensorflow and PyTorch
@@ -31,7 +31,7 @@ setup_seed(42)
 # Loading the Scikit-learn pipelines
 
 with open(
-    "./results/Models_WithinSession/Zhou2016/1/0/CSP + SVM/fitted_model_best.pkl",
+    "../how_to_benchmark/results/Models_WithinSession/Zhou2016/1/0/csp+svm/fitted_model_best.pkl",
     "rb",
 ) as pickle_file:
     CSP_SVM_Trained = load(pickle_file)
