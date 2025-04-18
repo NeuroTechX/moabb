f82b390a6b1bbe85a2af9214a8e5723c9466fbc1
Pierre Guetschel <25532709+PierreGtch@users.noreply.github.com>
2025-04-18 15:57:12 +0200

    Add `plot_all_datasets` function (#758)
    * Add plot_all_datasets function
    
    * Update example
    
    * Fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    * Update whatsnew
    
    * remove lambda
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- moabb/datasets/utils.py
+++ moabb/datasets/utils.py
@@ -3,6 +3,7 @@
 import inspect
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import mne
 import mne_bids
 import numpy as np
@@ -10,6 +11,7 @@ from mne import create_info
 from mne.io import RawArray
 
 import moabb.datasets as db
+from moabb.analysis.plotting import dataset_bubble_plot
 from moabb.datasets.base import BaseDataset
 from moabb.utils import aliases_list
 
@@ -351,3 +353,55 @@ def bids_metainfo(bids_path: Path) -> dict:
         json_data[uid]["fpath"] = str(path.fpath)
 
     return json_data
+
+
+def plot_all_datasets(height: float = 5, n_col: int = 10, **kwargs):
+    """Plots all the MOABB datasets in one figure.
+
+    This uses the :func:`~moabb.analysis.plotting.dataset_bubble_plot` function to
+    plot the datasets.
+    The datasets are sorted in alphabetical order and
+    plotted in a grid with n_col columns.
+
+    Parameters
+    ----------
+    height: float
+        Height of each subplot in inches.
+    n_col: int
+        Number of columns in the figure.
+    kwargs: dict
+        Additional arguments to pass to the dataset_bubble_plot function.
+
+    Returns
+    -------
+    fig: Figure
+        Pyplot handle
+    """
+    datasets = sorted(
+        [dataset() for dataset in dataset_list if "Fake" not in dataset.__name__],
+        key=lambda x: x.__class__.__name__,
+    )
+    fig, ax = plt.subplots(
+        1,
+        1,
+        figsize=(
+            height * (n_col),
+            height * (1 + len(datasets)) // n_col,
+        ),
+    )
+
+    def get_center(i):
+        return ((i % n_col) * height * 10, -(i // n_col) * height * 10)
+
+    lx, ly = get_center((len(datasets) // n_col + 1) * n_col - 1)
+    lx += height * 10
+    for i, dataset in enumerate(datasets):
+        dataset_bubble_plot(
+            dataset,
+            ax=ax,
+            center=get_center(i),
+            legend=i == len(datasets) - 1,
+            legend_position=(lx, ly),
+            **(kwargs or {}),
+        )
+    return fig
