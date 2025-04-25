0c0ec52294ecb0d29459787bd8b1924114ed3396
Alexander de Ranitz <89586187+alexander-de-ranitz@users.noreply.github.com>
2025-04-25 09:18:15 +0200

    Fix title formatting
    Co-authored-by: Bru <b.aristimunha@gmail.com>
    Signed-off-by: Alexander de Ranitz <89586187+alexander-de-ranitz@users.noreply.github.com>

--- examples/advanced_examples/plot_mne_features_pipeline.py
+++ examples/advanced_examples/plot_mne_features_pipeline.py
@@ -1,7 +1,7 @@
 """
-==========================
+==============================
 Pipelines using the mne-features library
-==========================
+==============================
 
 This example shows how to evaluate a pipeline constructed using the
 mne-features library
