b73121a64f5e596ba270fc30b6ab1e34942f3c83
Anton <toncho11@gmail.com>
2025-07-22 11:38:09 +0200

    small improvement

--- examples/tutorials/tutorial_6_use_an_X_y_dataset.py
+++ examples/tutorials/tutorial_6_use_an_X_y_dataset.py
@@ -159,4 +159,4 @@ pipelines["XD+TS+LR"] = make_pipeline(
 print("Start classification ...")
 scores = evaluation.process(pipelines)
 
-print("\n\nResults:", scores[["score", "time", "samples", "dataset", "pipeline"]])
+print("\n\nResults:\n", scores[["score", "time", "samples", "dataset", "pipeline"]])
