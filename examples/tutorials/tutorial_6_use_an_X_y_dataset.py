67ce234dddd12a8262c00c170786dc31c287c7d2
Anton <toncho11@gmail.com>
2025-07-22 10:49:11 +0200

    A new RawEpochParadigm that allows a non continuous dataset in the form of X y to be processed by MOABB.

---
+++ examples/tutorials/tutorial_6_use_an_X_y_dataset.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Sometimes we have the data in the format X y and not a continous signal. We 
+data in the form of epochs, not a continous signal. This creates a problem 
+becase MOABB is designed for continous data from subjects and sessions.
+
+The following example provides a special paradigm that uses an extra method on
+a dataset in order MOABB to be able to process data that comes as epochs.
+
+@author: Anton ANDREEV
+
+"""
+import numpy as np
+from moabb.datasets.base import BaseDataset
+from moabb.paradigms.base import BaseParadigm
+import pandas as pd
+from sklearn.metrics import make_scorer, accuracy_score
+from pyriemann.estimation import XdawnCovariances
+from sklearn.pipeline import make_pipeline
+from moabb.evaluations import (
+    WithinSessionEvaluation,
+)
+from sklearn.linear_model import LogisticRegression
+from pyriemann.tangentspace import TangentSpace
+
+class RawEpochParadigm(BaseParadigm):
+    """
+    A minimal paradigm that directly uses dataset.get_epoch_data()
+    with no filtering, epoching, or signal processing.
+    
+    Useful when your data is in the format X, y.
+    """
+
+    def __init__(self):
+        # filters=None indicates no filtering is done
+        super().__init__(filters=[])
+        self.return_epochs = False
+        
+    def get_data(self, dataset, subjects, return_epochs=False, **kwargs):
+        X_all, y_all, meta_all = [], [], []
+    
+        for subject in subjects:
+            X, y = dataset.get_epoch_data(subject)  # (n_trials, n_channels, n_times), (n_trials,)
+            
+            if isinstance(y, pd.Series):
+                y = y.values
+    
+            n_trials = len(y)
+            X_all.append(X)
+            y_all.append(y)
+    
+            # Build metadata for each trial, filling defaults for session and run
+            meta = pd.DataFrame({
+                "subject": [subject] * n_trials,
+                "session": [1] * n_trials,  # Default to 1 if sessions unknown
+                "run": [1] * n_trials,      # Default to 1 if runs unknown
+                "trial": list(range(n_trials)),
+                "label": y
+            })
+    
+            meta_all.append(meta)
+    
+        X = np.concatenate(X_all, axis=0)
+        y = np.concatenate(y_all, axis=0)
+        meta = pd.concat(meta_all, ignore_index=True)
+    
+        return X, y, meta
+
+    def is_valid(self, dataset):
+        return hasattr(dataset, "get_epoch_data")
+
+    @property
+    def scoring(self):
+        return make_scorer(accuracy_score)
+    
+    def used_events(self, dataset):
+    # Return event dict if needed, or {} if irrelevant
+        return {}
+
+    @property
+    def datasets(self):
+        return []
+    
+    def make_process_pipelines(self, dataset, **kwargs):
+    # Return a dummy no-op processing pipeline
+        return [lambda raw: raw]
+    
+class DummyRawEpochsDataset(BaseDataset):
+    """
+    Minimal custom dataset compatible with RawEpochParadigm.
+    """
+
+    def __init__(self, subjects=[1]):
+        super().__init__(
+            subjects=subjects,
+            sessions_per_subject=1,
+            events={"left": 0, "right": 1},  # required dummy event map
+            code="DummyRawEpochsDataset",
+            interval=[0, 1],
+            paradigm="RawEpochParadigm"
+        )
+        self.n_channels = 8
+        self.n_times = 128
+        self.n_trials = 100
+        self.n_classes = 2
+
+    def data_path(self, subject, path=None, force_update=False, update_path=True, verbose=None):
+        return None  # Not needed since we generate synthetic data
+
+    def _get_single_subject_data(self, subject):
+        raise NotImplementedError("Not used with RawEpochParadigm")
+
+    def get_epoch_data(self, subject=None):
+        """
+        Simulates epochs: shape (trials, channels, time), and labels
+        """
+        rng = np.random.default_rng(seed=subject)
+        X = rng.standard_normal((self.n_trials, self.n_channels, self.n_times))
+        y = rng.integers(low=0, high=self.n_classes, size=self.n_trials)
+        return X, y
+    
+dataset = DummyRawEpochsDataset()
+
+paradigm = RawEpochParadigm() #using the new special RawEpochParadigm paradigm
+
+X, labels, meta = paradigm.get_data(dataset=dataset, subjects=[1])
+
+# verify the correct number of trials (epochs)
+# if (X.shape[0] != len(dataset.get_epoch_data()[1])):
+#     raise Exception("Error in dataset/paradigm!")
+
+print("Epochs count before classfication",len(labels))
+
+evaluation = WithinSessionEvaluation(
+    paradigm=paradigm, datasets=dataset, overwrite=True, suffix="motan"
+)
+
+pipelines = {}
+
+pipelines["XD+TS+LR"]  = make_pipeline(
+                                    XdawnCovariances(nfilter=4, estimator="oas", xdawn_estimator="scm"),
+                                    TangentSpace(), 
+                                    LogisticRegression()
+                                    )
+
+print("Start classification ...")
+scores = evaluation.process(pipelines)
+
+#print results
+print(scores[['score', 'time','samples','dataset', 'pipeline']])
