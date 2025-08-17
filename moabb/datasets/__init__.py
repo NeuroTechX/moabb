8b95a59bfb1ac50f19c834e4f31fc4be69545473
Griffin <griffinbkeeler@gmail.com>
2025-08-17 14:03:36 -0500

    Updated references

--- moabb/datasets/__init__.py
+++ moabb/datasets/__init__.py
@@ -13,7 +13,7 @@ from . import compound_dataset
 from .alex_mi import AlexMI
 from .alphawaves import Rodrigues2017
 from .bbci_eeg_fnirs import Shin2017A, Shin2017B
-from .bcic import BCICIII_IVa
+from .bnci2003 import BNCI2003_IVa
 from .beetl import Beetl2021_A, Beetl2021_B
 
 # Depreciated datasets (will be removed in the future):
