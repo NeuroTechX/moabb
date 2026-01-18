f8dda55312d49c905ba8554af6b73791d44dc7b2
Bru <b.aristimunha@gmail.com>
2026-01-18 14:17:39 +0100

    Update moabb/benchmark.py
    Signed-off-by: Bru <b.aristimunha@gmail.com>

--- moabb/benchmark.py
+++ moabb/benchmark.py
@@ -36,7 +36,7 @@ log = logging.getLogger(__name__)
 
 def _ppl_needs_epochs(pn):
     """Check if the pipeline needs MNE epochs as input."""
-    ppl_with_epochs = ["braindecode", "Keras", "SSVEP CCA", "TRCA-SSVEP", "MsetCCA-SSVEP"]
+    ppl_with_epochs = ["SSVEP CCA", "TRCA-SSVEP", "MsetCCA-SSVEP"]
     if any(s in pn for s in ppl_with_epochs):
         return True
     else:
