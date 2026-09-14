22a96732f108f4bee8242f3ce71c7c870dae69ff
Bru <b.aristimunha@gmail.com>
2026-09-14 11:10:05 +0200

    Fix pyRiemann mean API compatibility in TRCA and TRCSP

--- moabb/pipelines/classification.py
+++ moabb/pipelines/classification.py
@@ -6,7 +6,7 @@ from joblib import Parallel, delayed
 from mne import BaseEpochs
 from pyriemann.estimation import Covariances, Shrinkage
 from pyriemann.geometry.covariance import covariances, normalize
-from pyriemann.geometry.mean import mean_covariance
+from pyriemann.geometry.mean import gmean
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.cross_decomposition import CCA
 from sklearn.preprocessing import LabelEncoder
@@ -681,7 +681,7 @@ class SSVEP_TRCA(BaseEstimator, ClassifierMixin):
         S = Shrinkage(shrinkage=0.01).fit_transform(S)
         S = normalize(S, "trace")
 
-        S_mean = mean_covariance(S, metric=self.method)
+        S_mean = gmean(S, metric=self.method)
 
         return S_mean, Q
 
